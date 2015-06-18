218846b2ce5d24104718a9538df234dfbb2cb179
Antoine Boulanger <hello@antoineboulanger.com>
2015-06-18 16:50:56 -0400

    Toggle nav user

---
+++ assets/src/scripts/charcoal/admin/template/menu.header.js
@@ -0,0 +1,12 @@
+Charcoal.Admin.Template_MenuHeader = function()
+{
+    $('[data-toggle="class"]').click(function(event) {
+        event.preventDefault();
+
+        var $this = $(this);
+        var dataClass = $this.data('class');
+        var dataTarget = $this.data('target');
+
+        $(dataTarget).toggleClass(dataClass);
+    });
+};

f48ead7a6545309cb705ba72a11e7191e746a395
stephenbe <stephenbe292+github@gmail.com>
2015-08-10 11:13:34 -0400

    Separated  css vendors and our code

--- grunt_tasks/sass.js
+++ grunt_tasks/sass.js
@@ -2,9 +2,15 @@ module.exports = {
     options: {
         sourceMap: false
     },
-    dist: {
+    app: {
         files: {
             'assets/dist/styles/charcoal.admin.css': 'assets/src/styles/**/charcoal.admin.scss'
         }
+    },
+    vendors: {
+        files: {
+            'assets/dist/styles/charcoal.admin.vendors.css': 'assets/src/styles/**/charcoal.admin.vendors.scss'
+        }
     }
+
 };

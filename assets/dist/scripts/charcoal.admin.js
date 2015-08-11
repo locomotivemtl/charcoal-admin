5b2dcef6920895b176243793dd2f212d85dce036
dlord <dlord@outlook.com>
2015-08-11 17:52:18 -0400

    Added render method to ComponentManager (creates components on DOM ready)

--- assets/dist/scripts/charcoal.admin.js
+++ assets/dist/scripts/charcoal.admin.js
@@ -82,27 +82,31 @@ Charcoal.Admin = (function ()
 
 Charcoal.Admin.ComponentManager = function ()
 {
-    this.input_properties = [];
-    this.widgets = [];
-    this.templates = [];
+    var that = this;
+
+    that.components = {};
+
+    $(document).on('ready', function () {
+        that.render();
+    });
 };
 
 Charcoal.Admin.ComponentManager.prototype.add_property_input = function (opts)
 {
-    this.add_component(this.input_properties, opts);
+    this.add_component('property_inputs', opts);
 };
 
 Charcoal.Admin.ComponentManager.prototype.add_widget = function (opts)
 {
-    this.add_component(this.widgets, opts);
+    this.add_component('widgets', opts);
 };
 
 Charcoal.Admin.ComponentManager.prototype.add_template = function (opts)
 {
-    this.add_component(this.templates, opts);
+    this.add_component('templates', opts);
 };
 
-Charcoal.Admin.ComponentManager.prototype.add_component = function (component_array, opts)
+Charcoal.Admin.ComponentManager.prototype.add_component = function (component_type, opts)
 {
     // Figure out which component to instanciate
     var ident = Charcoal.Admin.get_object_name(opts.type);
@@ -110,12 +114,35 @@ Charcoal.Admin.ComponentManager.prototype.add_component = function (component_ar
     // Make sure it exists first
     if (typeof(Charcoal.Admin[ident]) === 'function') {
 
-        var component = new Charcoal.Admin[ident](opts);
+        opts.ident = ident;
 
-        component_array.push(component);
+        // Check if component type array exists in components array
+        this.components[component_type] = this.components[component_type] || [];
+        this.components[component_type].push(opts);
 
     } else {
-        console.log('Was not able to instanciate ' + ident);
+        console.log('Was not able to store ' + ident + ' in ' + component_type + ' sub-array');
+    }
+
+};
+
+Charcoal.Admin.ComponentManager.prototype.render = function ()
+{
+
+    for (var component_type in this.components) {
+
+        for (var i = 0, len = this.components[component_type].length; i < len; i++) {
+
+            var component_data = this.components[component_type][i];
+
+            try {
+                var component = new Charcoal.Admin[component_data.ident](component_data);
+                this.components[component_type][i] = component;
+            } catch (error) {
+                console.log('Was not able to instanciate ' + component_data.ident);
+            }
+        }
+
     }
 };
 ;/**

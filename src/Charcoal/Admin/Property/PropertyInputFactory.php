6786d0bd29f53cee8c8ab920f5a300f73f6020cb
Mathieu Ducharme <mat@locomotive.ca>
2015-08-14 12:47:19 -0400

    Latest Factory

--- src/Charcoal/Admin/Property/PropertyInputFactory.php
+++ src/Charcoal/Admin/Property/PropertyInputFactory.php
@@ -7,34 +7,29 @@ use \Exception as Exception;
 use \Charcoal\Admin\Property\Input as Input;
 
 // From `charcoal-core`
-use \Charcoal\Core\AbstractFactory as AbstractFactory;
+use \Charcoal\Core\IdentFactory as IdentFactory;
 use \Charcoal\Property\PropertyInterface as PropertyInterface;
 
 /**
 *
 */
-class PropertyInputFactory extends AbstractFactory
+class PropertyInputFactory extends IdentFactory
 {
     /**
     * @param array $data
     */
     public function __construct(array $data = null)
     {
-        $this->set_factory_mode(AbstractFactory::MODE_IDENT);
         $this->set_base_class('\Charcoal\Admin\Property\PropertyInputInterface');
-
-        if ($data !== null) {
-            $this->set_data($data);
-        }
     }
 
     /**
-    * AbstractFactory > factory_class()
+    * IdentFactory > factory_class()
     *
     * @param string
     * @return string
     */
-    public function factory_class($class)
+    public function prepare_classname($class)
     {
         return $class.'Input';
     }

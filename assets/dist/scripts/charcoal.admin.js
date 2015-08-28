d2f37662806118323c1814047a385f1199c9f22d
beneroch <roch.bene@gmail.com>
2015-08-28 17:46:39 -0400

    Save value DB, this need some minors fixes from the gmap plugin

--- assets/dist/scripts/charcoal.admin.js
+++ assets/dist/scripts/charcoal.admin.js
@@ -204,19 +204,110 @@ Charcoal.Admin.ComponentManager.prototype.prepare_submit = function ()
 };
 ;/**
 * charcoal/admin/property
+* Should mimic the PHP equivalent AbstractProperty
+* This will prevent multiple directions in property implementation
+* by giving multiple usefull methods such as ident, val, etc.
 */
 
 Charcoal.Admin.Property = function (opts)
 {
-    window.alert('Property ' + opts);
+    this._ident = undefined;
+    this._val = undefined;
+    this._type = undefined;
+    this._input_type = undefined;
+
+    if (typeof opts.ident === 'string') {
+        this.set_ident(opts.ident);
+    }
+
+    if (typeof opts.val !== 'undefined') {
+        this.set_val(opts.val);
+    }
+
+    if (typeof opts.type !== 'undefined') {
+        this.set_type(opts.type);
+    }
+
+    if (typeof opts.input_type !== 'undefined') {
+        this.set_input_type(opts.input_type);
+    }
+
+    this.data = opts;
+
+    return this;
 };
 
 /**
-* Interface?
-* I suggest we use this for every property, just as PHP does
-* We do not have default action but return the current object
-* This will prevent further errors, and this will allow easy
-* validation throughout the form
+* Setters
+* The following are all defined setters we wanna use for all properties
+*/
+Charcoal.Admin.Property.prototype.set_ident = function (ident)
+{
+    this._ident = ident;
+};
+Charcoal.Admin.Property.prototype.set_val = function (val)
+{
+    this._val = val;
+};
+Charcoal.Admin.Property.prototype.set_type = function (type)
+{
+    this._type = type;
+};
+Charcoal.Admin.Property.prototype.set_input_type = function (input_type)
+{
+    this._input_type = input_type;
+};
+
+/**
+* Getters
+* The following are defined getters
+*/
+Charcoal.Admin.Property.prototype.ident         = function () {
+    return this._ident;
+};
+Charcoal.Admin.Property.prototype.val           = function () {
+    return this._val;
+};
+Charcoal.Admin.Property.prototype.type          = function () {
+    return this._type;
+};
+Charcoal.Admin.Property.prototype.input_type    = function () {
+    return this._input_type;
+};
+/**
+* Return the DOMElement element
+* @return {jQuery Object} $( '#' + this.data.id );
+* If not set, creates it
+*/
+Charcoal.Admin.Property.prototype.element = function ()
+{
+    if (!this._element) {
+        if (!this.data.id) {
+            // Error...
+            return false;
+        }
+        this._element = $('#' + this.data.id);
+    }
+    return this._element;
+};
+
+/**
+* Default validate action
+* Validate should return the validation feedback with a
+* success and / or message
+* IdeaS:
+* Use a validation object that has all necessary methods for
+* strings (max_length, min_length, etc)
+*
+* @return Object validation feedback
+*/
+Charcoal.Admin.Property.prototype.validate = function ()
+{
+    // Validate the current
+};
+
+/**
+* Default save action
 * @return this (chainable)
 */
 Charcoal.Admin.Property.prototype.save = function ()
@@ -973,22 +1064,18 @@ Charcoal.Admin.Property_Audio.prototype.update_analysers = function () {
 */
 Charcoal.Admin.Property_Input_Map_Widget = function (data)
 {
+    // Input type
+    data.input_type = 'charcoal/admin/property/input/map-widget';
+
+    Charcoal.Admin.Property.call(this, data);
+
     // Scope
     var that = this;
 
-    // Input type
-    this.input_type = 'charcoal/admin/property/input/map-widget';
-
     // Controller
     this._controller = undefined;
-
-    // HTML DOM element
-    this._element = undefined;
-
+    // Create uniq ident for every entities on the map
     this._object_inc = 0;
-
-    // Set options
-    this.data = data;
 
     if (typeof google === 'undefined') {
         // If google is undefined,
@@ -1043,22 +1130,29 @@ Charcoal.Admin.Property_Input_Map_Widget.prototype.init = function ()
         console.error('Missing ID');
     }
 
+    var map_options = {
+        use_clusterer: true,
+        map: {
+            center: {
+                x: 45.3712923,
+                y: -73.9820994
+            },
+            zoom: 14,
+            mapType: 'roadmap',
+            coordsType: 'inpage', // array, json? (vs ul li)
+            map_mode: 'default'
+        }
+    };
+
+    var current_value = this.element().find('input[type=hidden]').val();
+    if (current_value) {
+        map_options.places = JSON.parse(current_value);
+    }
+
     // Create new map instance
     this._controller = new window.BB.gmap.controller(
         this.element().find('.map-maker_map').get(0),
-        {
-            use_clusterer: true,
-            map: {
-                center: {
-                    x: 45.3712923,
-                    y: -73.9820994
-                },
-                zoom: 14,
-                mapType: 'roadmap',
-                coordsType: 'inpage', // array, json? (vs ul li)
-                map_mode: 'default'
-            }
-        }
+        map_options
     );
 
     // Init new map instance
@@ -1070,20 +1164,233 @@ Charcoal.Admin.Property_Input_Map_Widget.prototype.init = function ()
 };
 
 /**
-* Return the DOMElement element
-* @return {jQuery Object} $( '#' + this.data.id );
-* If not set, creates it
+* Return {BB.gmap.controller}
 */
-Charcoal.Admin.Property_Input_Map_Widget.prototype.element = function ()
+Charcoal.Admin.Property_Input_Map_Widget.prototype.controller = function ()
 {
-    if (!this._element) {
-        if (!this.data.id) {
-            // Error...
+    return this._controller;
+};
+
+/**
+* This is to prevent any ident duplication
+* Return {Int} Object index
+*/
+Charcoal.Admin.Property_Input_Map_Widget.prototype.object_index = function ()
+{
+    return ++this._object_inc;
+};
+
+/**
+* Return {BB.gmap.controller}
+*/
+Charcoal.Admin.Property_Input_Map_Widget.prototype.controls = function ()
+{
+    // Scope
+    var that = this;
+
+    var key = 'object';
+
+    this.element().on('click', '.js-add-marker', function (e)
+    {
+        e.preventDefault();
+
+        // already picked
+        if ($(this).hasClass('-active')) {
+            $(this).removeClass('-active');
             return false;
         }
-        this._element = $('#' + this.data.id);
+
+        // Active state
+        $(this).siblings('.-active').removeClass('-active');
+        $(this).addClass('-active');
+
+        that.controller().create_new('marker', key + that.object_index());
+    });
+
+    this.element().on('click', '.js-add-line', function (e)
+    {
+        e.preventDefault();
+
+        // already picked
+        if ($(this).hasClass('-active')) {
+            $(this).removeClass('-active');
+            return false;
+        }
+
+        // Active state
+        $(this).siblings('.-active').removeClass('-active');
+        $(this).addClass('-active');
+
+        that.controller().create_new('line', key + that.object_index());
+    });
+
+    this.element().on('click', '.js-add-polygon', function (e)
+    {
+        e.preventDefault();
+
+        // already picked
+        if ($(this).hasClass('-active')) {
+            $(this).removeClass('-active');
+            return false;
+        }
+
+        // Active state
+        $(this).siblings('.-active').removeClass('-active');
+        $(this).addClass('-active');
+
+        that.controller().create_new('polygon', key + that.object_index());
+    });
+
+    this.element().on('click', '.js-add_place_by_address', function (e) {
+        e.preventDefault();
+
+        var value = that.element().find('.js-address').val();
+        if (!value) {
+            // No value specified, no need to go further
+            return false;
+        }
+
+        that.controller().add_place_by_address('object' + that.object_index(), value, {
+            type: 'marker',
+            draggable: true,
+            // After loading the marker object
+            loaded_callback: function (marker) {
+                that.controller().map().setCenter(marker.object().getPosition());
+            }
+        });
+
+    });
+
+};
+
+/**
+* I believe this should fit the PHP model
+* Added the save() function to be called on form submit
+* Could be inherited from a global Charcoal.Admin.Property Prototype
+* Extra ideas:
+* - save
+* - validate
+* @return this (chainable)
+*/
+Charcoal.Admin.Property_Input_Map_Widget.prototype.save = function ()
+{
+    // Get raw map datas
+    var raw = this.controller().export();
+
+    // We might wanna save ONLY the places values
+    var places = (typeof raw.places === 'object') ? raw.places : {};
+
+    // Affect to the current property's input
+    // I see no reason to have more than one input hidden here.
+    // Split with classes or data if needed
+    this.element().find('input[type=hidden]').val(JSON.stringify(places));
+
+    return this;
+};
+/***
+* `charcoal/admin/property/input/map-widget`
+* Property_Input_Map_Widget Javascript class
+*
+*/
+Charcoal.Admin.Property_Input_Map_Widget = function (data)
+{
+    // Input type
+    data.input_type = 'charcoal/admin/property/input/map-widget';
+
+    Charcoal.Admin.Property.call(this, data);
+
+    // Scope
+    var that = this;
+
+    // Controller
+    this._controller = undefined;
+    // Create uniq ident for every entities on the map
+    this._object_inc = 0;
+
+    if (typeof google === 'undefined') {
+        // If google is undefined,
+        window._tmp_google_onload_function = function () {
+            that.load_plugin();
+        };
+
+        $.getScript(
+            'http://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false' +
+            '&language=fr&callback=_tmp_google_onload_function',
+            function () {}
+        );
+    } else {
+        that.init();
     }
-    return this._element;
+
+};
+
+Charcoal.Admin.Property_Input_Map_Widget.prototype = Object.create(Charcoal.Admin.Property.prototype);
+Charcoal.Admin.Property_Input_Map_Widget.prototype.constructor = Charcoal.Admin.Property_Input_Map_Widget;
+Charcoal.Admin.Property_Input_Map_Widget.prototype.parent = Charcoal.Admin.Property.prototype;
+
+Charcoal.Admin.Property_Input_Map_Widget.prototype.load_plugin = function ()
+{
+    // Scope
+    var that = this;
+
+    // Remove unnecessary tmp function
+    delete window._tmp_google_onload_function;
+
+    // Add the actual plugin
+    $.getScript(Charcoal.Admin.admin_url() +
+        // '../../vendor/locomotivemtl/charcoal-admin/bower_components/bb-gmap/assets/scripts/dist/bb.gmap.js',
+        '../../vendor/locomotivemtl/charcoal-admin/bower_components/bb-gmap/assets/scripts/dist/min/gmap.min.js',
+        function () {
+            that.init();
+        });
+};
+
+Charcoal.Admin.Property_Input_Map_Widget.prototype.init = function ()
+{
+    if (typeof BB === 'undefined' || typeof google === 'undefined') {
+        // We don't have what we need
+        console.error('Plugins not loaded');
+        return false;
+    }
+
+    var _data = this.data;
+
+    // Shouldn't happen at that point
+    if (typeof _data.id === 'undefined') {
+        console.error('Missing ID');
+    }
+
+    var map_options = {
+        use_clusterer: true,
+        map: {
+            center: {
+                x: 45.3712923,
+                y: -73.9820994
+            },
+            zoom: 14,
+            mapType: 'roadmap',
+            coordsType: 'inpage', // array, json? (vs ul li)
+            map_mode: 'default'
+        }
+    };
+
+    var current_value = this.element().find('input[type=hidden]').val();
+    if (current_value) {
+        map_options.places = JSON.parse(current_value);
+    }
+
+    // Create new map instance
+    this._controller = new window.BB.gmap.controller(
+        this.element().find('.map-maker_map').get(0),
+        map_options
+    );
+
+    // Init new map instance
+    this.controller().init();
+
+    // Start listeners for controls.
+    this.controls();
+
 };
 
 /**
--- assets/src/scripts/charcoal/admin/property.js
+++ assets/src/scripts/charcoal/admin/property.js
@@ -1,18 +1,109 @@
 /**
 * charcoal/admin/property
+* Should mimic the PHP equivalent AbstractProperty
+* This will prevent multiple directions in property implementation
+* by giving multiple usefull methods such as ident, val, etc.
 */
 
 Charcoal.Admin.Property = function (opts)
 {
-    window.alert('Property ' + opts);
+    this._ident = undefined;
+    this._val = undefined;
+    this._type = undefined;
+    this._input_type = undefined;
+
+    if (typeof opts.ident === 'string') {
+        this.set_ident(opts.ident);
+    }
+
+    if (typeof opts.val !== 'undefined') {
+        this.set_val(opts.val);
+    }
+
+    if (typeof opts.type !== 'undefined') {
+        this.set_type(opts.type);
+    }
+
+    if (typeof opts.input_type !== 'undefined') {
+        this.set_input_type(opts.input_type);
+    }
+
+    this.data = opts;
+
+    return this;
 };
 
 /**
-* Interface?
-* I suggest we use this for every property, just as PHP does
-* We do not have default action but return the current object
-* This will prevent further errors, and this will allow easy
-* validation throughout the form
+* Setters
+* The following are all defined setters we wanna use for all properties
+*/
+Charcoal.Admin.Property.prototype.set_ident = function (ident)
+{
+    this._ident = ident;
+};
+Charcoal.Admin.Property.prototype.set_val = function (val)
+{
+    this._val = val;
+};
+Charcoal.Admin.Property.prototype.set_type = function (type)
+{
+    this._type = type;
+};
+Charcoal.Admin.Property.prototype.set_input_type = function (input_type)
+{
+    this._input_type = input_type;
+};
+
+/**
+* Getters
+* The following are defined getters
+*/
+Charcoal.Admin.Property.prototype.ident         = function () {
+    return this._ident;
+};
+Charcoal.Admin.Property.prototype.val           = function () {
+    return this._val;
+};
+Charcoal.Admin.Property.prototype.type          = function () {
+    return this._type;
+};
+Charcoal.Admin.Property.prototype.input_type    = function () {
+    return this._input_type;
+};
+/**
+* Return the DOMElement element
+* @return {jQuery Object} $( '#' + this.data.id );
+* If not set, creates it
+*/
+Charcoal.Admin.Property.prototype.element = function ()
+{
+    if (!this._element) {
+        if (!this.data.id) {
+            // Error...
+            return false;
+        }
+        this._element = $('#' + this.data.id);
+    }
+    return this._element;
+};
+
+/**
+* Default validate action
+* Validate should return the validation feedback with a
+* success and / or message
+* IdeaS:
+* Use a validation object that has all necessary methods for
+* strings (max_length, min_length, etc)
+*
+* @return Object validation feedback
+*/
+Charcoal.Admin.Property.prototype.validate = function ()
+{
+    // Validate the current
+};
+
+/**
+* Default save action
 * @return this (chainable)
 */
 Charcoal.Admin.Property.prototype.save = function ()

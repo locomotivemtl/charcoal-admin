39f765f4a672770abb484c58bcbcb060b472d382
dominiclord <dlord@outlook.com>
2015-09-01 09:50:19 -0400

    Formatting Datetime properties for display

--- src/Charcoal/Admin/Property/Input/DatetimepickerInput.php
+++ src/Charcoal/Admin/Property/Input/DatetimepickerInput.php
@@ -11,25 +11,4 @@ use \Charcoal\Admin\Property\AbstractPropertyInput as AbstractPropertyInput;
 */
 class DatetimepickerInput extends AbstractPropertyInput
 {
-    /**
-    * Format `DateTime` to string.
-    *
-    * @todo   Adapt for l10n
-    * @return string
-    */
-    public function date_format()
-    {
-        return $this->p()->format();
-    }
-
-    /**
-    * Format `DateTime` to string.
-    *
-    * @todo   Adapt for l10n
-    * @return string|null
-    */
-    public function display_val()
-    {
-        return ($this->p()->val()) ? $this->p()->val()->format($this->date_format()) : null;
-    }
 }

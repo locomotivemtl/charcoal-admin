d783133fd05c5083591ec308ff5467f2bb74595c
Mathieu Ducharme <mat@locomotive.ca>
2015-10-05 11:35:25 -0400

    Rename graph widgets

--- src/Charcoal/Admin/Widget/Graph/Bar.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Charcoal\Admin\Widget\Graph;
-
-use \Charcoal\Admin\Widget\Graph as Graph;
-
-class Bar extends Graph
-{
-
-}
---
+++ src/Charcoal/Admin/Widget/Graph/BarWidget.php
@@ -0,0 +1,9 @@
+<?php
+namespace Charcoal\Admin\Widget\Graph;
+
+use \Charcoal\Admin\Widget\GraphWidget;
+
+class BarWidget extends GraphWidget
+{
+
+}

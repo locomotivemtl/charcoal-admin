25064720ca649cb793910af6f03e03c3fedf5886
Mathieu Ducharme <mat@locomotive.ca>
2015-10-08 21:36:53 -0400

    Fix phpunit bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,5 @@
 <?php
 
-use \Slim\Environment as SlimEnvironment;
-
-use \Charcoal\Admin\AdminModule as AdminModule;
-use \Charcoal\Charcoal as Charcoal;
-
 session_start();
 
 // Composer autoloader for Charcoal's psr4-compliant Unit Tests
@@ -12,15 +7,5 @@ $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->add('Charcoal\\Admin\\', __DIR__.'/../src/');
 $autoloader->add('Charcoal\\Admin\\Tests\\', __DIR__);
 
-// This var needs to be set automatically, for now
-Charcoal::init();
 
-// Set up the environment so that Slim can route
-Charcoal::app()->environment = SlimEnvironment::mock([
-    'REQUEST_METHOD'=>'get'
-]);
-
-
-$admin_module = new AdminModule();
-$admin_module->setup_routes();
 

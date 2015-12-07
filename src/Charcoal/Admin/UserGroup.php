74ca21d51ae3d25cf33387662f4918fb3cdc7bc0
Mathieu Ducharme <mat@locomotive.ca>
2015-12-07 14:38:29 -0500

    Add required key() method.

--- src/Charcoal/Admin/UserGroup.php
+++ src/Charcoal/Admin/UserGroup.php
@@ -6,5 +6,13 @@ use \Charcoal\Model\AbstractModel as AbstractModel;
 
 class UserGroup extends AbstractModel
 {
-
+    /**
+    * IndexableTrait > key()
+    *
+    * @return string
+    */
+    public function key()
+    {
+        return 'id';
+    }
 }

c41351b3ccd31a239ca3dd9aa8a0d1ff65b56543
Mathieu Ducharme <mat@locomotive.ca>
2015-12-08 08:52:22 -0500

    Fix cli login.

--- src/Charcoal/Admin/AdminScript.php
+++ src/Charcoal/Admin/AdminScript.php
@@ -2,11 +2,13 @@
 
 namespace Charcoal\Admin;
 
-// Intract-module (`charcoal-admin`) dependencies
-use \Charcoal\Admin\User as User;
+use \Exception;
 
 // Module `charcoal-app` dependencies
 use Charcoal\App\Script\AbstractScript;
+
+// Intract-module (`charcoal-admin`) dependencies
+use \Charcoal\Admin\User;
 
 /**
 *
@@ -47,40 +49,59 @@ abstract class AdminScript extends AbstractScript
 
         $u = User::get_authenticated();
         if ($u === null) {
-            $climate->yellow()->out('You need to be logged in into your "admin" account to continue...');
+            $climate->yellow()->out(
+                'You need to be logged in into your "admin" account to continue...'
+            );
 
-            $input = $climate->input('Please enter your username:');
+            $input = $climate->input(
+                'Please enter your username:'
+            );
             $username = $input->prompt();
-            $input = $climate->password('Please enter your password (hidden):');
+
+            $input = $climate->password(
+                'Please enter your password (hidden):'
+            );
             $password = $input->prompt();
+
             $climate->br();
 
-            $u = new User([
-                'logger'=>Charcoal::logger()
-            ]);
+            $this->logger()->debug(
+                sprintf('Admin login attempt: "%s"', $username)
+            );
+
             try {
+                $u = new User([
+                    'logger' => $this->logger()
+                ]);
                 $is_authenticated = $u->authenticate($username, $password);
             } catch (\Exception $e) {
                 $climate->dump($e);
                 $is_authenticated = false;
             }
+
             if (!$is_authenticated) {
-                $this->log_failed_attempt();
-                $climate->br()->error('Authentication failed.');
+                $this->log_failed_attempt($username);
+                $climate->br()->error(
+                    'Authentication failed.'
+                );
                 die();
             } else {
-                $this->log_successful_login();
+                $this->log_successful_login($username);
             }
         }
     }
 
-    public function log_failed_attempt()
+    public function log_failed_attempt($username)
     {
-
+        $this->logger()->warning(
+            sprintf('Login attempt failure: "%s"', $username)
+        );
     }
 
-    public function log_successful_login()
+    public function log_successful_login($username)
     {
-
+        $this->logger()->debug(
+            sprintf('Login attempt successful: "%s"', $username)
+        );
     }
 }

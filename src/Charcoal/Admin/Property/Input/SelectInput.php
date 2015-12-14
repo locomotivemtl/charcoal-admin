9e3c5a338dad7d795b8e722e7afc8e66e0195d27
Mathieu Ducharme <mat@locomotive.ca>
2015-12-14 11:44:21 -0500

    Fix select input to latest choice / selectable property features.

--- src/Charcoal/Admin/Property/Input/SelectInput.php
+++ src/Charcoal/Admin/Property/Input/SelectInput.php
@@ -15,6 +15,36 @@ class SelectInput extends AbstractPropertyInput
 
     public function choices()
     {
-        return $this->p()->choices();
+        $choices = $this->p()->choices();
+        foreach($choices as $choice_ident=>$choice) {
+            if(!isset($choice['value'])) {
+                $choice['value'] = $choice_ident;
+            }
+            if(!isset($choice['label'])) {
+                $choice['label'] = ucwords(strtolower(str_replace('_', ' ', $choice_ident)));
+            }
+            if(!isset($choice['title'])) {
+                $choice['title'] = $choice['label'];
+            }
+            $choice['selected'] = $this->is_choice_selected($choice_ident);
+
+            yield $choice;
+        }
+    }
+
+        /**
+    * @return boolean
+    */
+    public function is_choice_selected($c)
+    {
+        $val = $this->p()->val();
+        if ($val === null) {
+            return false;
+        }
+        if ($this->p()->multiple()) {
+            return in_array($c, $val);
+        } else {
+            return $c == $val;
+        }
     }
 }

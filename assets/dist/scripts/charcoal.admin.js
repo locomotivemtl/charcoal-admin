e64d0eed5c1ce70b6961351333938986a07320a1
Mathieu Ducharme <mat@locomotive.ca>
2016-02-08 11:24:27 -0500

    Generated js

--- assets/dist/scripts/charcoal.admin.js
+++ assets/dist/scripts/charcoal.admin.js
@@ -1484,6 +1484,50 @@ Charcoal.Admin.Property_Input_Audio.prototype.recording_update_analysers = funct
 
 })(window);
 ;/**
+* Color picker
+*
+* Require
+* - jquery-minicolors
+*/
+
+Charcoal.Admin.Property_Input_Colorpicker = function (opts)
+{
+    this.input_type = 'charcoal/admin/property/input/colorpicker';
+
+    // Property_Input_Colorpicker properties
+    this.input_id = null;
+    this.colorpicker_options = null;
+
+    this.set_properties(opts);
+    this.create_colorpicker();
+};
+
+Charcoal.Admin.Property_Input_Colorpicker.prototype = Object.create(Charcoal.Admin.Property.prototype);
+Charcoal.Admin.Property_Input_Colorpicker.prototype.constructor = Charcoal.Admin.Property_Input_Colorpicker;
+Charcoal.Admin.Property_Input_Colorpicker.prototype.parent = Charcoal.Admin.Property.prototype;
+
+Charcoal.Admin.Property_Input_Colorpicker.prototype.set_properties = function (opts)
+{
+    this.input_id = opts.id || this.input_id;
+    this.colorpicker_options = opts.data.colorpicker_options || this.colorpicker_options;
+
+    var default_opts = {
+        format: 'hex',
+        letterCase: 'uppercase',
+        opacity: false,
+        theme: 'bootstrap'
+    };
+
+    this.colorpicker_options = $.extend({}, default_opts, this.colorpicker_options);
+
+    return this;
+};
+
+Charcoal.Admin.Property_Input_Colorpicker.prototype.create_colorpicker = function ()
+{
+    $('#' + this.input_id).minicolors(this.colorpicker_options);
+};
+;/**
 * DateTime picker that manages datetime properties
 * charcoal/admin/property/input/datetimepicker
 *
@@ -1502,7 +1546,8 @@ Charcoal.Admin.Property_Input_DateTimePicker = function (opts)
     this.datetimepicker_selector = null;
     this.datetimepicker_options = null;
 
-    this.set_properties(opts).create_datetimepicker();
+    this.set_properties(opts);
+    this.create_datetimepicker();
 };
 Charcoal.Admin.Property_Input_DateTimePicker.prototype = Object.create(Charcoal.Admin.Property.prototype);
 Charcoal.Admin.Property_Input_DateTimePicker.prototype.constructor = Charcoal.Admin.Property_Input_DateTimePicker;

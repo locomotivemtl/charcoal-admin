ee2aeb69c012ded009d60a764825653cbd98abe9
Mathieu Ducharme <mat@locomotive.ca>
2016-02-18 13:17:01 -0500

    Optional background image or video for login.

--- src/Charcoal/Admin/Template/LoginTemplate.php
+++ src/Charcoal/Admin/Template/LoginTemplate.php
@@ -2,6 +2,8 @@
 
 namespace Charcoal\Admin\Template;
 
+use \Pimple\Container;
+
 // Local parent namespace dependencies
 use \Charcoal\Admin\AdminTemplate as AdminTemplate;
 
@@ -10,6 +12,44 @@ use \Charcoal\Admin\AdminTemplate as AdminTemplate;
  */
 class LoginTemplate extends AdminTemplate
 {
+    private $adminConfig;
+
+    public function setDependencies(Container $container)
+    {
+        $baseConfig = $container['config'];
+        $this->adminConfig = $baseConfig['admin'];
+    }
+
+    /**
+    * @return string
+    */
+    public function backgroundImage()
+    {
+        if (!isset($this->adminConfig['login'])) {
+            return '';
+        }
+        $loginConfig = $this->adminConfig['login'];
+        if (!isset($loginConfig['background_image']) || !is_string($loginConfig['background_image'])) {
+            return '';
+        }
+        return $loginConfig['background_image'];
+    }
+
+    /**
+    * @return string
+    */
+    public function backgroundVideo()
+    {
+        if (!isset($this->adminConfig['login'])) {
+            return '';
+        }
+        $loginConfig = $this->adminConfig['login'];
+        if (!isset($loginConfig['background_video']) || !is_string($loginConfig['background_video'])) {
+            return '';
+        }
+        return $loginConfig['background_video'];
+    }
+
     /**
      * Authentication is obviously never required for the login page.
      *

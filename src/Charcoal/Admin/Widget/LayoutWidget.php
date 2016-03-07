47337172a67ce1c5a1222804d2f51bea5da59f27
Mathieu Ducharme <mat@locomotive.ca>
2016-03-07 09:49:39 -0500

    Use layout from charcoal-ui

--- src/Charcoal/Admin/Widget/LayoutWidget.php
+++ src/Charcoal/Admin/Widget/LayoutWidget.php
@@ -5,10 +5,13 @@ namespace Charcoal\Admin\Widget;
 use \InvalidArgumentException;
 use \Iterator;
 
+// Dependencies from `charcoal-ui`
+use \Charcoal\Ui\Layout\LayoutInterface;
+use \Charcoal\Ui\Layout\LayoutTrait;
 
+// Intra-module (`charcoal-admin`) dependencies
 use \Charcoal\Admin\AdminWidget;
-use \Charcoal\Admin\Ui\LayoutInterface;
-use \Charcoal\Admin\Ui\LayoutTrait;
+
 
 /**
  * Layout Widget Controller

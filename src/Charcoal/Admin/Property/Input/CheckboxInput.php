3d9270c41a767a283ea7ebfdf7e14e38f1b60cc0
Chauncey McAskill <chauncey@locomotive.ca>
2016-06-13 17:31:49 -0400

    Added Selectable Property Input
    Similar to SelectablePropertyTrait; used by input properties which offer the choice of one or more values.
    
    Changes:
    - Fixed Mustache template for Radio Button input;
    - Updated Checkbox, Radio, and Select to extend AbstractSelectableInput;

--- src/Charcoal/Admin/Property/Input/CheckboxInput.php
+++ src/Charcoal/Admin/Property/Input/CheckboxInput.php
@@ -2,18 +2,40 @@
 
 namespace Charcoal\Admin\Property\Input;
 
-use \Charcoal\Admin\Property\AbstractPropertyInput;
+use \Charcoal\Admin\Property\AbstractSelectableInput;
 
 /**
- * Checkbox property input.
+ * Checkbox Input Property
+ *
+ * The HTML _check box_ (`<input type="checkbox">`) input element represents
+ * a control to select an array of different values.
+ * — {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox}
  */
-class CheckboxInput extends AbstractPropertyInput
+class CheckboxInput extends AbstractSelectableInput
 {
     /**
+     * Always accept multiple values.
+     *
      * @return boolean
      */
-    public function checked()
+    public function multiple()
     {
-        return !!$this->p()->val();
+        return true;
+    }
+
+    /**
+     * Prepare a single selectable option for output.
+     *
+     * @param  string|integer $ident  The choice key.
+     * @param  array|object   $choice The choice structure.
+     * @return array|null
+     */
+    protected function parseChoice($ident, $choice)
+    {
+        $choice = parent::parseChoice($ident, $choice);
+
+        $choice['inputId'] = $this->generateInputId();
+
+        return $choice;
     }
 }

be15da936fd1c5f1433b7266e2e19ac06aa1e1bf
Mathieu Ducharme <mat@locomotive.ca>
2016-07-27 12:00:21 -0400

    Show flags for preview, upload and picker.

--- src/Charcoal/Admin/Property/Input/FileInput.php
+++ src/Charcoal/Admin/Property/Input/FileInput.php
@@ -9,5 +9,78 @@ use \Charcoal\Admin\Property\AbstractPropertyInput;
  */
 class FileInput extends AbstractPropertyInput
 {
-    // Nothing to declare.
+    /**
+     * Flag wether the "file preview" should be displayed.
+     *
+     * @var bool $showFilePreview
+     */
+    private $showFilePreview = true;
+
+    /**
+     * Flag wether the "file upload" input should be displayed.
+     *
+     * @var bool $showFileUpload
+     */
+    private $showFileUpload = true;
+
+    /**
+     * Flag wether the "file picker" popup button should be displaed.
+     *
+     * @var bool $showFilePicker
+     */
+    private $showFilePicker = true;
+
+    /**
+     * @param boolean $show The show file preview flag.
+     * @return FileInput Chainable
+     */
+    public function setShowFilePreview()
+    {
+        $this->showFilePreview = !!$show;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function showFilePreview()
+    {
+        return $this->showFilePreview;
+    }
+
+    /**
+     * @param boolean $show The show file upload flag.
+     * @return FileInput Chainable
+     */
+    public function setShowFileUpload()
+    {
+        $this->showFileUpload = !!$show;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function showFileUpload()
+    {
+        return $this->showFileUpload;
+    }
+
+    /**
+     * @param boolean $show The show file picker flag.
+     * @return FileInput Chainable
+     */
+    public function setShowFilePicker()
+    {
+        $this->showFilePicker = !!$show;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function showFilePicker()
+    {
+        return $this->showFilePicker;
+    }
 }

37ded957a5e97c99a57522449eadc133dde25c42
Chauncey McAskill <shimoroka@gmail.com>
2016-07-27 01:18:38 -0400

    Added Boolean Display Property
    Changes:
    - Added color variants to ‘.badge’;
    - Fixed location of modifications to ‘.nav-tabs’;

---
+++ src/Charcoal/Admin/Property/Display/BooleanDisplay.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Charcoal\Admin\Property\Display;
+
+use \Charcoal\Admin\Property\AbstractPropertyDisplay;
+
+/**
+ * Boolean/Semaphore Display Property
+ */
+class BooleanDisplay extends AbstractPropertyDisplay
+{
+}

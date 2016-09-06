92e3bd408e7b6a9bbbc1a73af3f1b5e9d385c50b
Chauncey McAskill <chauncey@locomotive.ca>
2016-09-06 16:14:05 -0400

    Added support for customizing the SelectInput
    Changes:
    - Added “select_options” (similar to TinyMCE Input’s “editor_options”) for customizing the Bootstrap Select Picker;
    - Split SelectInput template into smaller parts for easier enhancements;
    - Fixed mixup between “title” and “label” properties;
    - Removed ‘selectpicker’ CSS class from ‘<select>’ to fix unapplied select picker options;

--- src/Charcoal/Admin/Property/Input/SelectInput.php
+++ src/Charcoal/Admin/Property/Input/SelectInput.php
@@ -13,6 +13,14 @@ use \Charcoal\Admin\Property\AbstractSelectableInput;
 class SelectInput extends AbstractSelectableInput
 {
     /**
+     * The Bootstrap select picker settigns.
+     *
+     * @var  array
+     * @link http://silviomoreto.github.io/bootstrap-select/
+     */
+    private $selectOptions;
+
+    /**
      * Retrieve the selectable options.
      *
      * @todo [^1]: With PHP7 we can simply do `yield from $choices;`.
@@ -49,4 +57,99 @@ class SelectInput extends AbstractSelectableInput
             'title'   => $label,
             'subtext' => ''
         ];
+    }
+
+    /**
+     * Set the select picker's options.
+     *
+     * This method overwrites existing helpers.
+     *
+     * @param array $opts The select picker options.
+     * @return Tinymce Chainable
+     */
+    public function setSelectOptions(array $opts)
+    {
+        $this->selectOptions = $opts;
+
+        return $this;
+    }
+
+    /**
+     * Merge (replacing or adding) select picker options.
+     *
+     * @param array $opts The select picker options.
+     * @return Tinymce Chainable
+     */
+    public function mergeSelectOptions(array $opts)
+    {
+        $this->selectOptions = array_merge($this->selectOptions, $opts);
+
+        return $this;
+    }
+
+    /**
+     * Add (or replace) an select picker option.
+     *
+     * @param string $optIdent The setting to add/replace.
+     * @param mixed  $optVal   The setting's value to apply.
+     * @throws InvalidArgumentException If the identifier is not a string.
+     * @return Tinymce Chainable
+     */
+    public function addSelectOption($optIdent, $optVal)
+    {
+        if (!is_string($optIdent)) {
+            throw new InvalidArgumentException(
+                'Option identifier must be a string.'
+            );
+        }
+
+        // Make sure default options are loaded.
+        if ($this->selectOptions === null) {
+            $this->selectOptions();
+        }
+
+        $this->selectOptions[$optIdent] = $optVal;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the select picker's options.
+     *
+     * @return array
+     */
+    public function selectOptions()
+    {
+        if ($this->selectOptions === null) {
+            $this->setSelectOptions($this->defaultSelectOptions());
+        }
+
+        return $this->selectOptions;
+    }
+
+    /**
+     * Retrieve the default select picker options.
+     *
+     * @return array
+     */
+    public function defaultSelectOptions()
+    {
+        $metadata = $this->metadata();
+
+        if (isset($metadata['data']['select_options'])) {
+            return $metadata['data']['select_options'];
+        }
+
+        return [];
+    }
+
+    /**
+     * Retrieve the select picker's options as a JSON string.
+     *
+     * @return string Returns data serialized with {@see json_encode()}.
+     */
+    public function selectOptionsAsJson()
+    {
+        return json_encode($this->selectOptions());
+    }
 }

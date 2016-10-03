228364e9211419f47878a15c73c14a4f2f71780b
Joel Alphonso <admin@joeldesign.ca>
2016-10-03 12:32:03 -0400

    Create iuser script can now be called from a static function like so: Create::start

--- src/Charcoal/Admin/Script/User/CreateScript.php
+++ src/Charcoal/Admin/Script/User/CreateScript.php
@@ -14,6 +14,15 @@ use \Charcoal\Admin\AdminScript;
  */
 class CreateScript extends AdminScript
 {
+    /**
+     * A way to create a user from a php script
+     * @return void
+     */
+    public static function start()
+    {
+        $script = new CreateScript();
+        $script->createUser();
+    }
 
     /**
      * @param RequestInterface  $request  A PSR-7 compatible Request instance.
@@ -24,13 +33,24 @@ class CreateScript extends AdminScript
     {
         unset($request);
 
+        $this->createUser();
+
+        return $response;
+    }
+
+    /**
+     * Create a new user in the database
+     * @return void
+     */
+    private function createUser()
+    {
         $climate = $this->climate();
 
         $climate->underline()->out(
             'Create a new Charcoal Administrator'
         );
 
-        $user = $this->modelFactory()->create('charcoal/admin/user');
+        $user       = $this->modelFactory()->create('charcoal/admin/user');
         $properties = $user->properties();
 
         $shown_props = [
@@ -46,15 +66,15 @@ class CreateScript extends AdminScript
                 continue;
             }
             if ($prop->type() == 'password') {
-                 $input = $climate->password(sprintf('Enter value for "%s":', $prop->label()));
+                $input = $climate->password(sprintf('Enter value for "%s":', $prop->label()));
             } else {
                 $input = $climate->input(sprintf('Enter value for "%s":', $prop->label()));
             }
             $input = $this->propertyToInput($prop);
-            $v = $input->prompt();
+            $v     = $input->prompt();
 
             $prop->setVal($v);
-            $valid = $prop->validate();
+            $valid                = $prop->validate();
             $vals[$prop->ident()] = $v;
         }
 
@@ -69,8 +89,6 @@ class CreateScript extends AdminScript
         } else {
             $climate->red()->out("\nError. Object could not be created.");
         }
-
-        return $response;
     }
 
     /**

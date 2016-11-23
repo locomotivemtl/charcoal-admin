da94cf588ca5b6a0ae5d1ba63232fdd3dcdfd05b
Chauncey McAskill <shimoroka@gmail.com>
2016-11-23 02:53:49 -0500

    Supporting checker pattern for Image Property
    Changes:
    - Cleaned up ImageDisplay;
    - Replaced manually managed list of inline styles with a setter/getter `styleAttr` which can be customized by the developer;
    - Added `classAttr` to ImageDisplay and ImageInput;
    - Added `.u-transparent` and `.u-contrast-dark` to improve visibility of images;

--- src/Charcoal/Admin/Property/Display/ImageDisplay.php
+++ src/Charcoal/Admin/Property/Display/ImageDisplay.php
@@ -10,40 +10,162 @@ use \Charcoal\Admin\Property\AbstractPropertyDisplay;
 class ImageDisplay extends AbstractPropertyDisplay
 {
     /**
-     * @var mixed $val
+     * The CSS classes for the HTML `class` attribute.
+     *
+     * @var string|null
      */
-    protected $val;
+    protected $classAttr;
 
     /**
-     * Width of the image
-     * @var string $width (ex: 100px)
+     * The CSS styling declarations for the HTML `style` attribute.
+     *
+     * @var string|null
+     */
+    protected $styleAttr;
+
+    /**
+     * The intrinsic width when displaying the image.
+     *
+     * @var string
      */
     protected $width = 'auto';
 
     /**
-     * Height of the image
-     * @var string $height (ex: 100px)
+     * The intrinsic height when displaying the image.
+     *
+     * @var string
      */
     protected $height = 'auto';
 
     /**
-     * Max-width of the image
-     * Default: none
-     * @var string $maxWidth (ex:100px)
+     * The maximum width to display the image.
+     *
+     * @var string
      */
     protected $maxWidth = 'none';
 
     /**
-     * Max-height of the image
-     * Default: none
-     * @var string $maxHeight (ex:100px)
+     * The maximum height to display the image.
+     *
+     * @var string
      */
     protected $maxHeight = 'none';
 
     /**
-     * Width setter
-     * If integer specified, 'px' will be append to it
-     * @param mixed $width CSS value.
+     * Set the CSS styling declarations to apply on the image.
+     *
+     * @param  string|string[] $styles An associative array of CSS styles.
+     * @return ImageDisplay Chainable
+     */
+    public function setStyleAttr($styles)
+    {
+        if (is_array($styles)) {
+            $styles = $this->parseStyleAttr($styles);
+        }
+
+        $defaults = $this->parseStyleAttr($this->defaultStyleAttr());
+        if ($defaults) {
+            $styles .= ' '.$defaults;
+        }
+
+        $this->styleAttr = $styles;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the CSS styling declarations for the HTML `style` attribute.
+     *
+     * @return string
+     */
+    public function styleAttr()
+    {
+        if ($this->styleAttr === null) {
+            $this->styleAttr = $this->parseStyleAttr($this->defaultStyleAttr());
+        }
+
+        return $this->styleAttr;
+    }
+
+    /**
+     * Parse the CSS styling declarations from the property's display features.
+     *
+     * @return string
+     */
+    protected function defaultStyleAttr()
+    {
+        return [
+            'height'     => $this->height(),
+            'width'      => $this->width(),
+            'max-height' => $this->maxHeight(),
+            'max-width'  => $this->maxWidth(),
+        ];
+    }
+
+    /**
+     * Parse the CSS styling declarations from the property's display features.
+     *
+     * @param  string[] $styles An associative array of CSS styles.
+     * @return string
+     */
+    protected function parseStyleAttr(array $styles)
+    {
+        $inline = array_map(
+            function ($val, $key) {
+                if (is_bool($val)) {
+                    return ($val) ? $key : '';
+                } elseif (isset($val)) {
+                    if (is_array($val)) {
+                        $val = implode(' ', $val);
+                    }
+
+                    $val = htmlspecialchars($val, ENT_QUOTES);
+
+                    if (is_string($val)) {
+                        return sprintf('%1$s: %2$s;', $key, $val);
+                    }
+                }
+            },
+            $styles,
+            array_keys($styles)
+        );
+
+        return implode(' ', $inline);
+    }
+
+    /**
+     * Set the CSS classes to apply on the image.
+     *
+     * @param  string|string[] $classes A space-separated list of CSS classes.
+     * @return ImageDisplay Chainable
+     */
+    public function setClassAttr($classes)
+    {
+        if (is_array($classes)) {
+            $classes = implode(' ', $classes);
+        }
+
+        $this->classAttr = $classes;
+
+        return $this;
+    }
+
+    /**
+     * Retrieve the CSS classes for the HTML `class` attribute.
+     *
+     * @return string
+     */
+    public function classAttr()
+    {
+        return $this->classAttr;
+    }
+
+    /**
+     * Set the display width of the image.
+     *
+     * If integer specified, 'px' will be append to it.
+     *
+     * @param  mixed $width A CSS value; a length, percentage or `calc()`.
      * @return ImageDisplay Chainable
      */
     public function setWidth($width)
@@ -51,13 +173,15 @@ class ImageDisplay extends AbstractPropertyDisplay
         if (is_numeric($width)) {
             $width .= 'px';
         }
+
         $this->width = $width;
+
         return $this;
     }
 
     /**
-     * Width getter
-     * Default @see $this->setWidth()
+     * Retrieve the display width of the image.
+     *
      * @return string
      */
     public function width()
@@ -66,9 +190,11 @@ class ImageDisplay extends AbstractPropertyDisplay
     }
 
     /**
-     * Height setter
-     * If integer specified, 'px' will be append to it
-     * @param string $height CSS value.
+     * Set the display height of the image.
+     *
+     * If integer specified, 'px' will be append to it.
+     *
+     * @param  mixed $height A CSS value; a length, percentage or `calc()`.
      * @return ImageDisplay Chainable
      */
     public function setHeight($height)
@@ -76,13 +202,15 @@ class ImageDisplay extends AbstractPropertyDisplay
         if (is_numeric($height)) {
             $height .= 'px';
         }
+
         $this->height = $height;
+
         return $this;
     }
 
     /**
-     * Height getter
-     * Default @see $this->setHeight()
+     * Retrieve the display width of the image.
+     *
      * @return string
      */
     public function height()
@@ -90,25 +218,28 @@ class ImageDisplay extends AbstractPropertyDisplay
         return $this->height;
     }
 
-
     /**
-     * Width setter
-     * If integer specified, 'px' will be append to it
-     * @param string $maxWidth CSS value.
+     * Set the maximum width to display the image.
+     *
+     * If integer specified, 'px' will be append to it.
+     *
+     * @param  mixed $width A CSS value; a length, percentage or `calc()`.
      * @return ImageDisplay Chainable
      */
-    public function setMaxWidth($maxWidth)
+    public function setMaxWidth($width)
     {
-        if (is_numeric($maxWidth)) {
-            $maxWidth .= 'px';
+        if (is_numeric($width)) {
+            $width .= 'px';
         }
-        $this->maxWidth = $maxWidth;
+
+        $this->maxWidth = $width;
+
         return $this;
     }
 
     /**
-     * MaxWidth getter
-     * Default @see $this->setMaxWidth()
+     * Retrieve the maximum width to display the image.
+     *
      * @return string
      */
     public function maxWidth()
@@ -117,27 +248,42 @@ class ImageDisplay extends AbstractPropertyDisplay
     }
 
     /**
-     * Height setter
-     * If integer specified, 'px' will be append to it
-     * @param string $maxHeight CSS value.
+     * Set the maximum height to display the image.
+     *
+     * If integer specified, 'px' will be append to it.
+     *
+     * @param  mixed $height A CSS value; a length, percentage or `calc()`.
      * @return ImageDisplay Chainable
      */
-    public function setMaxHeight($maxHeight)
+    public function setMaxHeight($height)
     {
-        if (is_numeric($maxHeight)) {
-            $maxHeight .= 'px';
+        if (is_numeric($height)) {
+            $height .= 'px';
         }
-        $this->maxHeight = $maxHeight;
+
+        $this->maxHeight = $height;
+
         return $this;
     }
 
     /**
-     * Height getter
-     * Default @see $this->setMaxHeight()
+     * Retrieve the maximum height to display the image.
+     *
      * @return string
      */
     public function maxHeight()
     {
         return $this->maxHeight;
+    }
+
+    /**
+     * Determine if the value is a {@see @see http://en.wikipedia.org/wiki/Data_URI_scheme Data URI}.
+     *
+     * @param  string $val A path or URI to analyze.
+     * @return boolean
+     */
+    protected function isDataUri($val)
+    {
+        return (0 === strpos($val, 'data:'));
     }
 }

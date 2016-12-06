ca35354e5f342fec0a488f5f8312118722e31f54
Mathieu Ducharme <mat@locomotive.ca>
2016-12-06 13:52:19 -0500

    Class has moved.

--- src/Charcoal/Admin/Template/LoginTemplate.php
+++ src/Charcoal/Admin/Template/LoginTemplate.php
@@ -7,7 +7,7 @@ use \Charcoal\Translation\TranslationString;
 
 // From 'charcoal-admin'
 use \Charcoal\Admin\AdminTemplate;
-use \Charcoal\Admin\Object\AuthToken;
+use \Charcoal\Admin\User\AuthToken;
 
 /**
  *

7452394bb7d0706cafc16cb68b565b18cbde674c
Joel Alphonso <joel@locomotive.ca>
2016-12-13 14:13:10 -0500

    quick fix for metadata key ** TODO : to be removed when key can be set from metadata.

--- src/Charcoal/Admin/User/AclRole.php
+++ src/Charcoal/Admin/User/AclRole.php
@@ -9,5 +9,16 @@ use \Charcoal\User\AclRole as UserAclRole;
  */
 class AclRole extends UserAclRole
 {
-
+    /**
+     * Override key to ident
+     * Temp fix
+     *
+     * todo remove when metadata key can be used.
+     *
+     * @return string
+     */
+    public function key()
+    {
+        return 'ident';
+    }
 }

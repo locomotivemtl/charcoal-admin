fc9da0c32ce0feb285436a824e6a3810d3750129
Mathieu Ducharme <mat@locomotive.ca>
2017-01-10 23:10:27 -0500

    Use ACL Manager

--- src/Charcoal/Admin/ServiceProvider/AclServiceProvider.php
+++ src/Charcoal/Admin/ServiceProvider/AclServiceProvider.php
@@ -2,18 +2,16 @@
 
 namespace Charcoal\Admin\ServiceProvider;
 
-// Dependencies from `ext-pdo`
-use \PDO;
-use \PDOException;
-
 // Dependencies from `pimple/pimple`
 use \Pimple\Container;
 use \Pimple\ServiceProviderInterface;
 
 // Dependencies from `zendframework/zend-permissions`
 use \Zend\Permissions\Acl\Acl;
-use \Zend\Permissions\Acl\Role\GenericRole as Role;
-use \Zend\Permissions\Acl\Resource\GenericResource as Resource;
+use \Zend\Permissions\Acl\Resource\GenericResource as AclResource;
+
+// Dependencies from `charcoal-base`
+use \Charcoal\User\Acl\Manager as AclManager;
 
 /**
  * Admin ACL (Access-Control-List) provider.
@@ -24,14 +22,16 @@ use \Zend\Permissions\Acl\Resource\GenericResource as Resource;
  *
  * - `admin/acl` A Zend ACL instance containing the admin resources / permissions.
  *
- * ## Helpers
  *
- * - `admin/acl/roles`
- * - `admin/acl/permissions`
+ * ## Dependencies
+ *
+ * This service provider expects a few "global" services to be registered on the container:
+ * - `logger`, a PSR-3 logger
+ * - `database`, a PDO instance
+ * - `admin/config`, a PSR-6 Cache pool
  */
 class AclServiceProvider implements ServiceProviderInterface
 {
-
     /**
      * @param Container $container Pimple DI Container.
      * @return void
@@ -39,130 +39,35 @@ class AclServiceProvider implements ServiceProviderInterface
     public function register(Container $container)
     {
         /**
-         * @param Container $container Pimple DI container
-         * @return array
-         */
-        $container['admin/acl/roles'] = function (Container $container) {
-
-            $db = $container['database'];
-
-            $q = '
-                SELECT
-                    `ident`,
-                    `parent`
-                FROM
-                    `charcoal_admin_acl_roles`
-                ORDER BY
-                    `position` ASC';
-
-            $container['logger']->debug($q);
-
-            // Put inside a try-catch block because ACL is optional; table might not exist.
-            try {
-                $sth = $db->query($q);
-                $roles = $sth->fetchAll(PDO::FETCH_KEY_PAIR);
-            } catch (PDOException $e) {
-                $container['logger']->warning('Can not fetch ACL roles: '.$e->getMessage());
-                $roles = [];
-            }
-
-            return $roles;
-        };
-
-        /**
-         * @param Container $container Pimple DI container
-         * @return array
-         */
-        $container['admin/acl/permissions'] = function (Container $container) {
-
-            $db = $container['database'];
-
-            $q = '
-                SELECT
-                    `ident`,
-                    `denied`,
-                    `allowed`,
-                    `superuser`
-                FROM
-                    `charcoal_admin_acl_roles`
-                ORDER BY
-                    `position` ASC';
-
-            $container['logger']->debug($q);
-
-            $permissions = [];
-            // Put inside a try-catch block because ACL is optional; table might not exist.
-            try {
-                $sth = $db->query($q);
-                while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-                    $ident = $row['ident'];
-                    if ($row['superuser']) {
-                        $permissions[$ident] = 'superuser';
-                    } else {
-                        $allowed = explode(',', trim($row['allowed']));
-                        $denied = explode(',', trim($row['denied']));
-                        $permissions[$ident] = [
-                            'allow' => $allowed,
-                            'deny'  => $denied
-                        ];
-                    }
-                }
-            } catch (PDOException $e) {
-                $container['logger']->warning('Can not fetch ACL roles: '.$e->getMessage());
-                $permissions = [];
-            }
-
-            return $permissions;
-        };
-
-        /**
+         * Use an AclManager to load default permissions from config and database.
+         *
          * @param Container $container Pimple DI container
          * @return Acl
          */
         $container['admin/acl'] = function (Container $container) {
+
+            $adminConfig = $container['admin/config'];
+
+            $resourceName = 'admin';
+            $tableName = 'charcoal_admin_acl_roles';
+
+            $aclManager = new AclManager([
+                'logger'    => $container['logger']
+            ]);
+
             $acl = new Acl();
 
-            // Default role, which will contain default permissions
-            $acl->addRole(new Role('default'));
-
-            // Add roles
-            $roles = $container['admin/acl/roles'];
-            foreach ($roles as $role => $parentRole) {
-                if ($parentRole) {
-                    $acl->addRole(new Role($role), $parentRole);
-                } else {
-                    $acl->addRole(new Role($role), 'default');
-                }
-            }
-
-            // Add resources
-            $acl->addResource(new Resource('admin'));
+             // Add admin resource for ACL
+            $acl->addResource(new AclResource($resourceName));
 
             // Setup default permissions (from admin config)
-            $adminConfig = $container['admin/config'];
-            $defaultPermissions = $adminConfig['acl.default_permissions'];
-            foreach ($defaultPermissions['allowed'] as $allowed) {
-                $acl->allow('default', 'admin', $allowed);
-            }
-            foreach ($defaultPermissions['denied'] as $denied) {
-                $acl->deny('default', 'admin', $denied);
+            $permissions = $adminConfig['acl.permissions'];
+            if (!empty($permissions)) {
+                $aclManager->loadPermissions($acl, $permissions, $resourceName);
             }
 
-            // Add permissions
-            $permissions = $container['admin/acl/permissions'];
-            foreach ($permissions as $role => $permissions) {
-                if ($permissions === 'superuser') {
-                    // Allow all
-                    $acl->allow($role);
-                } else {
-                    foreach ($permissions['allow'] as $allowed) {
-                        $acl->allow($role, 'admin', $allowed);
-                    }
-                    foreach ($permissions['deny'] as $denied) {
-                        $acl->deny($role, 'admin', $denied);
-                    }
-                }
-            }
+            // Setup roles and permissions from database
+            $aclManager->loadDatabasePermissions($acl, $container['database'], $tableName, $resourceName);
 
             return $acl;
         };

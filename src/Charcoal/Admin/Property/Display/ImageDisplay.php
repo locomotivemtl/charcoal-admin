342cfdff476eec0a4f2c75bb5db4242e2d481779
Joel Alphonso <joel@locomotive.ca>
2017-01-13 09:37:16 -0500

    Amend 9df2ef62: Missing "base-url"

--- src/Charcoal/Admin/Property/Display/ImageDisplay.php
+++ src/Charcoal/Admin/Property/Display/ImageDisplay.php
@@ -4,6 +4,7 @@ namespace Charcoal\Admin\Property\Display;
 
 use \Charcoal\Admin\Ui\ImageAttributesTrait;
 use \Charcoal\Admin\Property\AbstractPropertyDisplay;
+use Pimple\Container;
 
 /**
  * Image Display Property
@@ -11,6 +12,26 @@ use \Charcoal\Admin\Property\AbstractPropertyDisplay;
 class ImageDisplay extends AbstractPropertyDisplay
 {
     use ImageAttributesTrait;
+
+    /**
+     * The base URI for the Charcoal application.
+     *
+     * @var string|\Psr\Http\Message\UriInterface
+     */
+    public $baseUrl;
+
+    /**
+     * Inject dependencies from a DI Container.
+     *
+     * @param Container $container A dependencies container instance.
+     * @return void
+     */
+    public function setDependencies(Container $container)
+    {
+        parent::setDependencies($container);
+
+        $this->baseUrl = $container['base-url'];
+    }
 
     /**
      * Retrieve display value.
@@ -23,6 +44,7 @@ class ImageDisplay extends AbstractPropertyDisplay
 
         if (!parse_url($val, PHP_URL_SCHEME)) {
             $uri = $this->baseUrl;
+
             return $uri->withPath($val);
         }
 

d35cec0d55aac10f243943782caf25a0d7584861
Chauncey McAskill <chauncey@locomotive.ca>
2017-03-22 18:03:26 -0400

    Improved Radio/Checkbox Color Inputs
    Changes:
    - Revised styles to support block or inline layout;
    - Added support for “color” alias to tickable color input choices;
    - Added support for “show_label” feature for tickable color input choices;

--- src/Charcoal/Admin/Property/Input/CheckboxColorInput.php
+++ src/Charcoal/Admin/Property/Input/CheckboxColorInput.php
@@ -7,4 +7,26 @@ namespace Charcoal\Admin\Property\Input;
  */
 class CheckboxColorInput extends CheckboxInput
 {
+    /**
+     * Prepare a single tickable option for output.
+     *
+     * @see    RadioColorInput::parseChocie() For similar features.
+     * @param  string|integer $ident  The choice key.
+     * @param  array|object   $choice The choice structure.
+     * @return array|null
+     */
+    protected function parseChoice($ident, $choice)
+    {
+        $choice = parent::parseChoice($ident, $choice);
+
+        if (!isset($choice['color'])) {
+            $choice['color'] = $choice['value'];
+        }
+
+        if (!isset($choice['show_label'])) {
+            $choice['show_label'] = false;
+        }
+
+        return $choice;
+    }
 }

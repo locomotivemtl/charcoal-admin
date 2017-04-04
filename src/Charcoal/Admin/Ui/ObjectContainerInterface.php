ad9b101efc1aa8bf646f5e78b5fff7cd24e55917
Chauncey McAskill <chauncey@locomotive.ca>
2017-04-04 13:36:31 -0400

    Added ‘ObjectContainerTrait::hasObj()’

--- src/Charcoal/Admin/Ui/ObjectContainerInterface.php
+++ src/Charcoal/Admin/Ui/ObjectContainerInterface.php
@@ -10,28 +10,45 @@ namespace Charcoal\Admin\Ui;
 interface ObjectContainerInterface
 {
     /**
-     * @param string $objType The object type.
+     * Set the object type.
+     *
+     * @param  string $objType The object type.
      * @return ObjectContainerInterface Chainable
      */
     public function setObjType($objType);
 
     /**
-     * @return string
+     * Retrieve the object type.
+     *
+     * @return string|null
      */
     public function objType();
 
     /**
-     * @param mixed $objId The object id, to load.
+     * Set the object ID.
+     *
+     * @param  mixed $objId The object ID to load.
      * @return ObjectContainerInterface Chainable
      */
     public function setObjId($objId);
 
     /**
+     * Retrieve the object ID.
+     *
      * @return mixed
      */
     public function objId();
 
     /**
+     * Determine if the class has a concrete object.
+     *
+     * @return boolean
+     */
+    public function hasObj();
+
+    /**
+     * Retrieve the object.
+     *
      * @return ModelInterface
      */
     public function obj();

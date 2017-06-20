03380653b7a769101d12fd6dcf209d3644e68460
Joel Alphonso <joel@locomotive.ca>
2017-06-20 17:29:34 -0400

    Rework of selectize/loadAction to use p()->choice from selectizeInput instead of loading the collection like the default loadAction() This is to ensure all the same filters and orders are applied the same way when the collection is loaded using the LoadAction()

--- src/Charcoal/Admin/Action/Selectize/LoadAction.php
+++ src/Charcoal/Admin/Action/Selectize/LoadAction.php
@@ -5,6 +5,8 @@ namespace Charcoal\Admin\Action\Selectize;
 use Charcoal\Admin\Action\Object\LoadAction as DefaultLoadAction;
 use Charcoal\Admin\Service\SelectizeRenderer;
 use Pimple\Container;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Selectize Load Action
@@ -12,6 +14,13 @@ use Pimple\Container;
 class LoadAction extends DefaultLoadAction
 {
     use SelectizeRendererAwareTrait;
+
+    /**
+     * The collection to return.
+     *
+     * @var array|mixed
+     */
+    private $selectizeCollection;
 
     /**
      * Dependencies
@@ -27,20 +36,90 @@ class LoadAction extends DefaultLoadAction
     }
 
     /**
-     * Fetch ids from Object Collection.
-     *
-     * @return array
+     * @param  RequestInterface  $request  The request options.
+     * @param  ResponseInterface $response The response to return.
+     * @return ResponseInterface
+     * @throws UnexpectedValueException If "obj_id" is passed as $request option.
+     * @todo   Implement obj_id support for load object action
      */
-    private function parseCollectionIds()
+    public function run(RequestInterface $request, ResponseInterface $response)
     {
-        $collection = $this->objCollection();
-        $ids = [];
+        $failMessage = $this->translator()->translation('Failed to load object(s)');
+        $errorThrown = strtr($this->translator()->translation('{{ errorMessage }}: {{ errorThrown }}'), [
+            '{{ errorMessage }}' => $failMessage
+        ]);
+        $reqMessage  = $this->translator()->translation(
+            '{{ parameter }} required, must be a {{ expectedType }}, received {{ actualType }}'
+        );
 
-        foreach ($collection as $object) {
-            $ids[] = $object->id();
+        $this->setData($request->getParams());
+
+        $objType = $this->selectizeObjType();
+
+        if (!$objType) {
+            $actualType = is_object($objType) ? get_class($objType) : gettype($objType);
+            $this->addFeedback('error', strtr($reqMessage, [
+                '{{ parameter }}'    => '"obj_type"',
+                '{{ expectedType }}' => 'string',
+                '{{ actualType }}'   => $actualType,
+            ]));
+            $this->setSuccess(false);
+
+            return $response->withStatus(400);
         }
 
-        return $ids;
+        try {
+            $selectizeInput = $this->selectizeInput();
+            $choices = $selectizeInput->p()->choices();
+            $this->setSelectizeCollection($this->selectizeVal($choices));
+
+            $count = count($choices);
+            switch ($count) {
+                case 0:
+                    $doneMessage = $this->translator()->translation('No objects found.');
+                    break;
+
+                case 1:
+                    $doneMessage = $this->translator()->translation('One object found.');
+                    break;
+
+                default:
+                    $doneMessage = strtr($this->translator()->translation('{{ count }} objects found.'), [
+                        '{{ count }}' => $count
+                    ]);
+                    break;
+            }
+            $this->addFeedback('success', $doneMessage);
+            $this->setSuccess(true);
+
+            return $response;
+        } catch (Exception $e) {
+            $this->addFeedback('error', strtr($errorThrown, [
+                '{{ errorThrown }}' => $e->getMessage()
+            ]));
+            $this->setSuccess(false);
+
+            return $response->withStatus(500);
+        }
+    }
+
+    /**
+     * @return array|mixed
+     */
+    public function selectizeCollection()
+    {
+        return $this->selectizeCollection;
+    }
+
+    /**
+     * @param array|mixed $selectizeCollection The collection to return.
+     * @return self
+     */
+    public function setSelectizeCollection($selectizeCollection)
+    {
+        $this->selectizeCollection = $selectizeCollection;
+
+        return $this;
     }
 
     /**
@@ -48,12 +127,10 @@ class LoadAction extends DefaultLoadAction
      */
     public function results()
     {
-        $results = parent::results();
-
-        if ($this->success() === true) {
-            $results['selectize'] = $this->selectizeVal($this->parseCollectionIds());
-        }
-
-        return $results;
+        return [
+            'success'    => $this->success(),
+            'feedbacks'  => $this->feedbacks(),
+            'selectize'  => $this->selectizeCollection()
+        ];
     }
 }

2e60e8d135f19eb9a77c48a7fbedc804623d2906
Mathieu Ducharme <mat@locomotive.ca>
2017-11-01 00:38:28 -0400

    Add (basic) validation to user creation script. Allow custom labels for script inputs.

--- src/Charcoal/Admin/Script/User/CreateScript.php
+++ src/Charcoal/Admin/Script/User/CreateScript.php
@@ -2,18 +2,23 @@
 
 namespace Charcoal\Admin\Script\User;
 
+use Exception;
+
 // PSR-7 (http messaging) dependencies
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 // Intra-module (`charcoal-admin`) dependencies
 use Charcoal\Admin\AdminScript;
+use Charcoal\Admin\User;
 
 /**
  * Create admin user script.
  */
 class CreateScript extends AdminScript
 {
+    const MIN_PASSWORD_LENGTH = 5;
+
     /**
      * @param array|\ArrayAccess $data The dependencies (app and logger).
      */
@@ -72,7 +77,12 @@ class CreateScript extends AdminScript
     {
         unset($request);
 
-        $this->createUser();
+        try {
+            $this->createUser();
+        } catch (Exception $e) {
+            $this->climate()->red()->out("\nError. User could not be created");
+            $this->climate()->red()->out($e->getMessage());
+        }
 
         return $response;
     }
@@ -83,62 +93,150 @@ class CreateScript extends AdminScript
      */
     private function createUser()
     {
-        $climate = $this->climate();
-
-        $climate->underline()->out(
-            'Create a new Charcoal Administrator'
+        $this->climate()->underline()->out(
+            $this->translator()->translate('Create a new Charcoal Administrator User')
         );
 
-        $user       = $this->modelFactory()->create('charcoal/admin/user');
-        $properties = $user->properties();
-
-        $shown_props = [
-            'username',
-            'email',
-            'roles',
-            'password'
-        ];
-
-        $default_props = [
-            'username' => $climate->arguments->get('username'),
-            'email'    => $climate->arguments->get('email'),
-            'password' => $climate->arguments->get('password'),
-            'roles'    => $climate->arguments->get('roles'),
-        ];
+        $user       = $this->modelFactory()->create(User::class);
+        $prompts = $this->userPrompts();
+        $properties = $user->properties(array_keys($prompts));
 
         $vals = [];
         foreach ($properties as $prop) {
-            if (!in_array($prop->ident(), $shown_props)) {
+            if (!in_array($prop->ident(), array_keys($prompts))) {
                 continue;
             }
 
-            if ($default_props[$prop->ident()]) {
-                $v = $default_props[$prop->ident()];
+            $prompt = $prompts[$prop->ident()];
+
+            if ($prompt['property']) {
+                $v = $prompt['property'];
             } else {
-                if ($prop->type() == 'password') {
-                    $input = $climate->password(sprintf('Enter value for "%s":', $prop->label()));
-                } else {
-                    $input = $climate->input(sprintf('Enter value for "%s":', $prop->label()));
-                }
-                $input = $this->propertyToInput($prop);
-                $v     = $input->prompt();
+                $v = $this->promptProperty($prop, $prompt['label']);
+            }
+            if (isset($prompt['validation'])) {
+                call_user_func($prompt['validation'], $v);
             }
 
             $prop->setVal($v);
-            $valid                = $prop->validate();
             $vals[$prop->ident()] = $v;
         }
 
+        // Trigger reset password
         $user->resetPassword($vals['password']);
         unset($vals['password']);
 
         $user->setFlatData($vals);
-
         $ret = $user->save();
+
         if ($ret) {
-            $climate->green()->out("\n".sprintf('Success! User "%s" created.', $ret));
+            $this->climate()->green()->out("\n".sprintf('Success! User "%s" created.', $ret));
         } else {
-            $climate->red()->out("\nError. Object could not be created.");
+            $this->climate()->red()->out("\nError. User could not be created.");
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function userPrompts()
+    {
+        return [
+            'username' => [
+                'label' => $this->translator()->translate('Please enter username: '),
+                'property' => $this->climate()->arguments->get('username'),
+                'validation' => [$this, 'validateUsername']
+            ],
+            'email'    => [
+                'label' => $this->translator()->translate('Please enter email: '),
+                'property' => $this->climate()->arguments->get('email'),
+                'validation' => [$this, 'validateEmail']
+            ],
+            'password' => [
+                'label' => $this->translator()->translate('Please enter password: '),
+                'property' => $this->climate()->arguments->get('password'),
+                'validation' => [$this, 'validatePassword']
+            ],
+            'roles'    => [
+                'label' => $this->translator()->translate('Please enter role(s) [ex: admin], comma separated: '),
+                'property' => $this->climate()->arguments->get('roles'),
+                'validation' => null
+            ]
+        ];
+    }
+
+    /**
+     * @param mixed  $prop  The property.
+     * @param string $label The label.
+     * @return mixed
+     */
+    private function promptProperty($prop, $label)
+    {
+        $input = $this->propertyToInput($prop, $label);
+        $v     = $input->prompt();
+        if ($prop->type() == 'password') {
+            $this->climate()->dim()->out('');
+        }
+
+        return $v;
+    }
+
+    /**
+     * @param string $username The username, from input.
+     * @throws Exception If the username is empty or already exists in the database.
+     * @return void
+     */
+    private function validateUsername($username)
+    {
+        if (!$username) {
+            throw new Exception(
+                $this->translator()->translate('Username can not be empty.')
+            );
+        }
+        $user       = $this->modelFactory()->create(User::class);
+        $user->load($username);
+        if ($user->username()) {
+            throw new Exception(
+                sprintf($this->translator()->translate('Username "%s" already exists in database.'), $username)
+            );
+        }
+    }
+
+    /**
+     * @param string $email The email, from input.
+     * @throws Exception If the email is empty or invalid (validated with php's filters).
+     * @return void
+     */
+    private function validateEmail($email)
+    {
+        if (!$email) {
+            throw new Exception(
+                $this->translator()->translate('Email can not be empty.')
+            );
+        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception(
+                sprintf($this->translator()->translate('Invalid email "%s".'), $email)
+            );
+        }
+    }
+
+    /**
+     * @param string $password The password, from input.
+     * @throws Exception If the password is empty or too small.
+     * @return void
+     */
+    private function validatePassword($password)
+    {
+        if (!$password) {
+            throw new Exception(
+                $this->translator()->translate('Password can not be empty.')
+            );
+        }
+        if (mb_strlen($password) < self::MIN_PASSWORD_LENGTH) {
+            throw new Exception(
+                $this->translator()->translate(sprintf('Password must be at least %d characters.', self::MIN_PASSWORD_LENGTH))
+            );
         }
     }
 }

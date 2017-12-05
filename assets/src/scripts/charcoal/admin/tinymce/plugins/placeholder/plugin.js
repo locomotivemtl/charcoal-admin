63e1d3003a7d62646166d07bb7a55d02ee224b92
Chauncey McAskill <chauncey@locomotive.ca>
2017-12-05 18:35:41 -0500

    Fix TinyMCE placeholder plugin
    Refactored to work with TinyMCE 4 and Inlite theme.

--- assets/src/scripts/charcoal/admin/tinymce/plugins/placeholder/plugin.js
+++ assets/src/scripts/charcoal/admin/tinymce/plugins/placeholder/plugin.js
@@ -1,68 +1,93 @@
 /**
  * Placeholder Text for TinyMCE
  *
- * @link https://github.com/mohan/tinymce-placeholder
+ * Based on {@link https://github.com/mohan/tinymce-placeholder Mohan's implementation}
  */
 
 (function (tinymce) {
 
-    var default_placeholder_attrs = {
-        class: 'mce-placeholder-area mce-content-body'
-    };
-
     tinymce.PluginManager.add('placeholder', function (editor) {
         editor.on('init', function () {
-            var placeholder_text = editor.getElement().getAttribute('placeholder') || editor.settings.placeholder;
-            if (placeholder_text === '' || placeholder_text === null || placeholder_text === undefined) {
+            var placeholder = editor.getElement().getAttribute('placeholder') || editor.settings.placeholder;
+            if (
+                typeof placeholder === 'undefined' ||
+                placeholder === null ||
+                placeholder === '' ||
+                placeholder === [] ||
+                placeholder === {}
+            ) {
                 return;
             }
 
+            if (typeof placeholder === 'string') {
+                placeholder = {
+                    text: placeholder
+                };
+            }
+
+            if (typeof placeholder.attr === 'undefined') {
+                placeholder.attr = editor.settings.placeholder_attr || { class: 'mce-placeholder-area' };
+            }
+            if (typeof placeholder.tag === 'undefined') {
+                placeholder.tag = editor.settings.placeholder_tag  || 'label';
+            }
+
             var label = new Label(
-                placeholder_text,
-                editor.settings.placeholder_attrs || default_placeholder_attrs,
-                editor.getContentAreaContainer()
+                placeholder,
+                editor.getContentAreaContainer() || editor.getBody()
             );
 
-            onBlur();
-
             tinymce.DOM.bind(label.el, 'click', onFocus);
+
+            // When focus is in main editor window
             editor.on('focus', onFocus);
+
+            // When focus is outside of main editor area
             editor.on('blur', onBlur);
-            editor.on('change', onBlur);
-            editor.on('setContent', onBlur);
-            editor.on('keydown', onKeydown);
+
+            // Whenever content is changed, including when a toolbar item is pressed (bold, italic, bullets, etc)
+            editor.on('change', onChange);
+
+            // Called when switching between Visual/Text
+            editor.on('setcontent', onSetContent);
 
             function onFocus() {
-                if (!editor.settings.readonly) {
-                    label.hide();
-                }
-
-                editor.execCommand('mceFocus', false, editor.id);
+                console.log('onFocus', label.text);
+                label.hide();
+                editor.focus();
             }
 
             function onBlur() {
-                if (editor.getContent() === '') {
-                    label.show();
-                } else {
-                    label.hide();
-                }
+                console.log('onBlur', label.text);
+                label.check();
             }
 
-            function onKeydown() {
-                label.hide();
+            function onChange() {
+                console.log('onChange', label.text);
+                label.check();
             }
+
+            function onSetContent() {
+                console.log('onSetContent', label.text);
+                label.check();
+            }
+
+            // Add 1 second timeout to delay execution until after
+            // vendor plugings adjust the toolbars
+            setTimeout(function () {
+                console.log('timeOut', label.text);
+                label.check();
+            }, 1000);
         });
 
-        var Label = function (text, attrs, area) {
+        var Label = function (data, area) {
+            this.data = data;
+            this.text = data.text;
+
             tinymce.DOM.setStyle(area, 'position', 'relative');
 
             // Create label el
-            this.el = tinymce.DOM.add(
-                area,
-                editor.settings.placeholder_tag || 'label',
-                attrs,
-                tinymce.DOM.decode(text)
-            );
+            this.el = tinymce.DOM.add(area, data.tag, data.attr, tinymce.DOM.decode(data.text));
         };
 
         Label.prototype.hide = function () {
@@ -71,6 +96,15 @@
 
         Label.prototype.show = function () {
             tinymce.DOM.setStyle(this.el, 'display', '');
+        };
+
+        Label.prototype.check = function () {
+            var textContent = editor.getBody().textContent.replace(this.el.textContent, '').trim();
+            if (textContent === '') {
+                this.show();
+            } else {
+                this.hide();
+            }
         };
     });
 

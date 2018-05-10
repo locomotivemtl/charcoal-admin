993ba82faf4ba6523ccb238e8de1e8170ca3c4f6
Joel Alphonso <joel@locomotive.ca>
2018-05-10 14:07:14 -0400

    Add CDN cache purgeability to ClearCacheAction.

--- src/Charcoal/Admin/Action/System/ClearCacheAction.php
+++ src/Charcoal/Admin/Action/System/ClearCacheAction.php
@@ -54,6 +54,13 @@ class ClearCacheAction extends AdminAction
             $this->cache->deleteItem('metadata');
             $this->addFeedback('success', $this->translator()->translate('Objects cache cleared successfully.'));
             $this->setSuccess(true);
+        } elseif ($cacheType === 'cdn') {
+            if (!$this->purgeCdnCache()) {
+                $this->setSuccess(false);
+                return $response->withStatus(400);
+            }
+            $this->addFeedback('success', $this->translator()->translate('Cdn cache cleared successfully.'));
+            $this->setSuccess(true);
         } else {
             $this->addFeedback('error', $this->translator()->translate(sprintf('Invalid cache type "%s"', $cacheType)));
             $this->setSuccess(false);
@@ -94,5 +101,70 @@ class ClearCacheAction extends AdminAction
     private function setCache(CacheItemPoolInterface $cache)
     {
         $this->cache = $cache;
+    }
+
+    private function purgeCdnCache()
+    {
+        $cdnConfig = $this->appConfig('cdn');
+
+        if (!$cdnConfig) {
+            $this->addFeedback('error', $this->translator()->translate('No cdn config found'));
+            return false;
+        }
+
+        if ($cdnConfig) {
+            $type = $cdnConfig['type'];
+
+            switch ($type) {
+                case 'cloudflare':
+                    return $this->purgeCloudflareCache();
+                    break;
+                default:
+                    $this->addFeedback('error', $this->translator()->translate(
+                        sprintf('The cdn type "%s" is not supported', $type)
+                    ));
+                    return false;
+            }
+        }
+    }
+
+    private function purgeCloudflareCache()
+    {
+        $api = $this->appConfig('apis.cloudflare');
+
+        if (!$api) {
+            $this->addFeedback('error', $this->translator()->translate(
+                sprintf('No api config found for "%s" cdn', 'cloudflare')
+            ));
+            return false;
+        }
+
+        $url = 'https://api.cloudflare.com/client/v4/zones/023e105f4ecef8ad9ca31a8372d0c353/purge_cache';
+        $header = [
+            'Content-Type: application/json',
+            sprintf('X-Auth-Email: %s', $api['auth_email']),
+            sprintf('X-Auth-Key: %s', $api['auth_key']),
+        ];
+        $fields = [
+            'purge_everything' => true
+        ];
+
+        //open connection
+        $ch = curl_init();
+
+        //set options
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //needed so that the $result=curl_exec() output is the file and isn't just true/false
+
+        //execute post
+        $result = curl_exec($ch);
+
+        //close connection
+        curl_close($ch);
+
+        return $result['success'];
     }
 }

5f73fa82691668ff003963789a3b71606a743eef
Chauncey McAskill <chauncey@locomotive.ca>
2018-05-16 17:24:44 -0400

    Improve ClearCache template and action (Take 4)
    Deprecated:
    - `data-purge="cache"` is now used for purging the cache; use `data-clear="cache"` to empty the cache
    - Route `system/clear-cache` in favour of `system/cache/clear`
    
    Added:
    - `AbstractCacheAction` to serve as base class for action controllers
    - `PurgeCacheAction` for purging old cache items
    - Route `system/cache/clear`
    - Route `system/cache/purge`
    
    Changed:
    - `ClearCacheAction` to extend new `AbstractCacheAction`
    - `ClearCacheAction` to decouple clearing routines and improve response handling
    - JS Cache component to support clearing AND purging operations
    - "Clear Global Cache" button
    
    Fixed:
    - `ClearCacheAction` to check the result of clear/delete operations on the cache pool
    
    Todo:
    - Support specific cache item deletion

---
+++ src/Charcoal/Admin/Action/System/PurgeCacheAction.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Charcoal\Admin\Action\System;
+
+// From PSR-7
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+// From 'charcoal-admin'
+use Charcoal\Admin\Action\System\AbstractCacheAction;
+
+/**
+ * Purge stale or expired items.
+ */
+class PurgeCacheAction extends AbstractCacheAction
+{
+    /**
+     * @param  RequestInterface  $request  A PSR-7 compatible Request instance.
+     * @param  ResponseInterface $response A PSR-7 compatible Response instance.
+     * @return ResponseInterface
+     */
+    public function run(RequestInterface $request, ResponseInterface $response)
+    {
+        $cache  = $this->cachePool();
+        $result = $cache->purge();
+
+        if ($result) {
+            $message = $this->translator()->translate('Cache purged successfully.');
+        } else {
+            $message = $this->translator()->translate('Failed to purge cache.');
+        }
+
+        $this->setSuccess($result);
+
+        if ($result) {
+            $this->addFeedback('success', $message);
+            return $response;
+        } else {
+            $this->addFeedback('error', $message);
+            return $response->withStatus(500);
+        }
+    }
+}

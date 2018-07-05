7c2bb152471829fcb81933239b8b3c93a88e5992
Joel Alphonso <joel@locomotive.ca>
2018-07-05 15:20:46 -0400

    Add CheckboxBtnGroup

---
+++ src/Charcoal/Admin/Property/Input/CheckboxBtnGroupInput.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Charcoal\Admin\Property\Input;
+
+// From 'charcoal-admin'
+use Charcoal\Admin\Property\Input\CheckboxInput;
+
+/**
+ * Radio Button as a Bootstrap Button Group
+ *
+ * @link https://getbootstrap.com/docs/4.0/components/button-group/
+ */
+class CheckboxBtnGroupInput extends CheckboxInput
+{
+
+}

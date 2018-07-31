001e28695ff22550987902e5b6f24c54ae3aca15
Joel Alphonso <joel@locomotive.ca>
2018-07-31 13:31:58 -0400

    HierarchicalObjectProperty should know the current object id since it's trying to be hierarchical.
    This is done through request param

--- src/Charcoal/Admin/Property/HierarchicalObjectProperty.php
+++ src/Charcoal/Admin/Property/HierarchicalObjectProperty.php
@@ -17,11 +17,39 @@ use Charcoal\Property\ObjectProperty;
 // From 'charcoal-support'
 use Charcoal\Object\HierarchicalCollection;
 
+// local dependencies
+use Charcoal\Admin\Support\HttpAwareTrait;
+
 /**
  * Hierarchical Object Property
  */
 class HierarchicalObjectProperty extends ObjectProperty
 {
+    use HttpAwareTrait;
+
+    const DATA_SOURCE_REQUEST = 'request';
+    const DATA_SOURCE_OBJECT  = 'object';
+
+    /**
+     * The property's object id.
+     *
+     * @var string|int
+     */
+    private $objId;
+
+    /**
+     * @param array $data The widget data.
+     * @return self
+     */
+    public function setData(array $data)
+    {
+        parent::setData($data);
+
+        $this->mergeDataSources($data);
+
+        return $this;
+    }
+
     /**
      * Retrieve the available choice structures, segmented as a tree.
      *
@@ -73,5 +101,84 @@ class HierarchicalObjectProperty extends ObjectProperty
         }
 
         return $choice;
+    }
+
+    /**
+     * Retrieve the available data sources (when setting data on an entity).
+     *
+     * @param array|mixed $dataset The entity data.
+     * @return self
+     */
+    protected function mergeDataSources($dataset = null)
+    {
+        $sources = [static::DATA_SOURCE_REQUEST];
+        foreach ($sources as $sourceIdent) {
+            $getter = $this->camelize('data_from_'.$sourceIdent);
+            $method = [ $this, $getter ];
+
+            if (is_callable($method)) {
+                $data = call_user_func($method);
+
+                if ($data) {
+                    parent::setData($data);
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Fetch metadata from the current request.
+     *
+     * @return array
+     */
+    protected function dataFromRequest()
+    {
+        return $this->httpRequest()->getParams($this->acceptedRequestData());
+    }
+
+    /**
+     * Retrieve the accepted metadata from the current request.
+     *
+     * @return array
+     */
+    protected function acceptedRequestData()
+    {
+        return [
+            'obj_id'
+        ];
+    }
+
+    /**
+     * @return integer|string The property object's id.
+     */
+    public function objId()
+    {
+        return $this->objId;
+    }
+
+    /**
+     * @param integer|string $objId ObjId for ObjectProperty.
+     * @return self
+     */
+    public function setObjId($objId)
+    {
+        $this->objId = $objId;
+
+        return $this;
+    }
+
+    /**
+     * Inject dependencies from a DI Container.
+     *
+     * @param  Container $container A dependencies container instance.
+     * @return void
+     */
+    protected function setDependencies(Container $container)
+    {
+        parent::setDependencies($container);
+
+        $this->setHttpRequest($container['request']);
     }
 }

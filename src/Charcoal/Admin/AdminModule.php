b204c20b6366a948dd430bdecebcbd67988bae86
Chauncey McAskill <chauncey@locomotive.ca>
2019-11-17 16:25:26 -0500

    Revert "Fix admin module exclusion from route to allow URLs such as /administration"
    This reverts commit b541b1f414ec146e896f8316de692b5b9fc67f82.

--- src/Charcoal/Admin/AdminModule.php
+++ src/Charcoal/Admin/AdminModule.php
@@ -36,13 +36,9 @@ class AdminModule extends AbstractModule
      */
     public function setup()
     {
+        // Hack: skip if the request does not start with '/admin'
         $container = $this->app()->getContainer();
-
-        // Hack: skip if the request does not start with '/admin[/]?'
-        // Make sure URL like '/administration' are still
-        // accessible and not processed as admin routes.
-        $trimmedPath = ltrim($container['request']->getUri()->getPath(), '/');
-        if ($trimmedPath !== 'admin' && substr($trimmedPath, 0, 6) !== 'admin/') {
+        if ($this->isPathAdmin($container['request']->getUri()->getPath()) !== true) {
             return $this;
         }
 

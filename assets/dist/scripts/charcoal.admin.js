21060d52a16fed48d61fc1c29d883237d7261055
Dominic Lord <dlord@outlook.com>
2020-03-16 16:20:05 -0400

    Build scripts

--- assets/dist/scripts/charcoal.admin.js
+++ assets/dist/scripts/charcoal.admin.js
@@ -10792,11 +10792,11 @@ Charcoal.Admin.Property_Input_Selectize_Tags.prototype.init_clipboard = function
  */
 
 Charcoal.Admin.Property_Input_Text = function (opts) {
+    Charcoal.Admin.Property.call(this, opts);
+
     this.input_type = 'charcoal/admin/property/input/text';
     this.opts       = opts;
     this.data       = opts.data;
-
-    Charcoal.Admin.Property.call(this, opts);
 
     // Required
     this.set_input_id(this.opts.id);

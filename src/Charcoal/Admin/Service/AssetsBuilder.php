7ed4d1c9ab01e706d52e9e514370248c2e598580
Joel Alphonso <joel@locomotive.ca>
2020-11-12 14:51:50 -0500

    fix(asset-manage): fix merging issues by allowing namespaced files array
    deprecated : defining files array directly under collection ident

--- src/Charcoal/Admin/Service/AssetsBuilder.php
+++ src/Charcoal/Admin/Service/AssetsBuilder.php
@@ -8,7 +8,6 @@ use Assetic\Asset\AssetReference;
 use Assetic\Asset\FileAsset;
 use Assetic\Asset\GlobAsset;
 use Assetic\AssetManager;
-
 // from charcoal-admin
 use Charcoal\Admin\AssetsConfig;
 
@@ -71,33 +70,50 @@ final class AssetsBuilder
      */
     private function parseCollections(array $collections)
     {
-        $basePath = dirname(__DIR__, 7).'/';
-
         foreach ($collections as $collectionIdent => $actions) {
-            $collection = [];
-
-            if (isset($actions['files'])) {
-                foreach ($actions['files'] as $f) {
-                    // Files with asterisks should be treated as glob.
-                    if (strpos($f, '*') !== false) {
-                        $collection[] = new GlobAsset($basePath.$f);
-                        continue;
-                    }
-
-                    // Files starting with '@' should be treated as assets reference.
-                    if ($f[0] === '@') {
-                        $f = ltrim($f, '@');
-
-                        $collection[] = new AssetReference($this->am, $f);
-                        continue;
-                    }
-
-                    $collection[] = new FileAsset($basePath.$f);
+            $files = ($actions['files'] ?? []);
+            // Parse scoped files. Solves merging issues.
+            array_walk($actions, function ($scope) use (&$files) {
+                if (isset($scope['files']) && !empty($scope['files'])) {
+                    $files = array_merge($files, $scope['files']);
                 }
-            }
+            });
+
+            $files = array_unique($files);
+            $collection = $this->extractFiles($files);
 
             $ac = new AssetCollection($collection);
             $this->am->set($collectionIdent, $ac);
         }
+    }
+
+    /**
+     * @param array $files Files to convert to Collection assets.
+     * @return array
+     */
+    private function extractFiles(array $files = [])
+    {
+        $basePath = dirname(__DIR__, 7).'/';
+        $collection = [];
+
+        foreach ($files as $f) {
+            // Files with asterisks should be treated as glob.
+            if (strpos($f, '*') !== false) {
+                $collection[] = new GlobAsset($basePath.$f);
+                continue;
+            }
+
+            // Files starting with '@' should be treated as assets reference.
+            if ($f[0] === '@') {
+                $f = ltrim($f, '@');
+
+                $collection[] = new AssetReference($this->am, $f);
+                continue;
+            }
+
+            $collection[] = new FileAsset($basePath.$f);
+        }
+
+        return $collection;
     }
 }

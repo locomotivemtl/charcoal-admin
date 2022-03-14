bc87db90cc8534e7b9a67d7a12760806f066a4c7
Michel Descoteaux <michel@locomotive.ca>
2022-03-14 09:32:20 -0400

    Search.js - Escape single-quote characters

--- assets/src/scripts/charcoal/admin/widget/search.js
+++ assets/src/scripts/charcoal/admin/widget/search.js
@@ -131,6 +131,7 @@ Charcoal.Admin.Widget_Search.prototype.parse_search_filters = function (query) {
         props = this.data.properties || [];
         $.each(words, function (i, word) {
             sub_filters = [];
+            word = word.replace(/'/g,'\\\'');
             $.each(props, function (j, prop) {
                 sub_filters.push({
                     property: prop,

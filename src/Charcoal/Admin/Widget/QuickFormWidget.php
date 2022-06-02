cb4910b5843119feef0aba6888086f597853c3ae
Xavier <xavier@locomotive.ca>
2022-06-02 10:58:57 -0400

    1st version of new group_display_mode for quick-form

--- src/Charcoal/Admin/Widget/QuickFormWidget.php
+++ src/Charcoal/Admin/Widget/QuickFormWidget.php
@@ -11,6 +11,41 @@ use Charcoal\Admin\Widget\ObjectFormWidget;
 class QuickFormWidget extends ObjectFormWidget
 {
     /**
+     * Ident for tab display.
+     *
+     * @const string
+     */
+    const DISPLAY_MODE_TAB = 'tab';
+    /**
+     * Ident for lang tab display.
+     *
+     * @const string
+     */
+    const DISPLAY_MODE_LANG = 'lang';
+
+    /**
+     * Whether to display the language switcher.
+     *
+     * @var boolean
+     */
+    protected $showLanguageSwitch;
+
+    /**
+     * @param  array $data The widget data.
+     * @return FormWidget Chainable
+     */
+    public function setData(array $data)
+    {
+        parent::setData($data);
+
+        if ($this->groupDisplayMode() === self::DISPLAY_MODE_LANG) {
+            $this->setTabsTemplate('charcoal/admin/template/form/nav-tabs-languages');
+        }
+
+        return $this;
+    }
+
+    /**
      * Retrieve the identifier of the form to use, or its fallback.
      *
      * @see    ObjectFormWidget::formIdentFallback()
@@ -48,6 +83,146 @@ class QuickFormWidget extends ObjectFormWidget
         }
 
         return parent::submitLabel();
+    }
+
+    /**
+     * Determine if the form has any multilingual properties.
+     *
+     * @return boolean
+     */
+    public function hasL10nFormProperties()
+    {
+        if ($this->validateObjType()) {
+            $locales = count($this->translator()->availableLocales());
+            if ($locales > 1) {
+                $obj = $this->obj();
+
+                $props = [];
+
+                foreach ($this->groups as $group) {
+                    array_push($props, ...($group->groupProperties()));
+                }
+
+                $props = array_values(array_unique($props));
+
+                foreach ($props as $prop) {
+                    $modelProp = $obj->property($prop);
+
+                    if ($modelProp['l10n']) {
+                        return true;
+                    } elseif ($modelProp instanceof ModelStructureProperty) {
+                        $metadata = $modelProp->getStructureMetadata();
+                        foreach ($metadata->properties() as $prop) {
+                            if (isset($prop['l10n']) && $prop['l10n']) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        return parent::hasL10nFormProperties();
+    }
+
+    /**
+     * @return boolean
+     */
+    public function showLanguageSwitch()
+    {
+        if ($this->showLanguageSwitch === null) {
+            $this->showLanguageSwitch = $this->hasL10nFormProperties();
+        }
+        return $this->showLanguageSwitch;
+    }
+
+    /**
+     * Determine if content groups are to be displayed as languages tabbable panes.
+     *
+     * @return boolean
+     */
+    public function isDisplayModeLang()
+    {
+        return ($this->groupDisplayMode() === self::DISPLAY_MODE_LANG) ? true : false;
+    }
+
+    /**
+     * Determine if content groups are to be displayed as tabbable panes.
+     *
+     * @return boolean
+     */
+    public function isTabbable()
+    {
+        $tabbableDisplayModes = [
+            self::DISPLAY_MODE_TAB,
+            self::DISPLAY_MODE_LANG
+        ];
+        return in_array($this->groupDisplayMode(), $tabbableDisplayModes) ? true : false;
+    }
+
+    /**
+     * @return array
+     */
+    public function availableLanguages()
+    {
+        $currentLocale = $this->translator()->getLocale();
+        $locales = $this->translator()->locales();
+        $languages = [];
+
+        uasort($locales, [ $this, 'sortLanguagesByPriority' ]);
+
+        foreach ($locales as $locale => $localeStruct) {
+            /**
+             * @see \Charcoal\Admin\Widget\FormGroupWidget::languages()
+             * @see \Charcoal\Property\LangProperty::localeChoices()
+             */
+            if (isset($localeStruct['name'])) {
+                $label = $this->translator()->translation($localeStruct['name']);
+            } else {
+                $trans = 'locale.'.$locale;
+                if ($trans === $this->translator()->translate($trans)) {
+                    $label = strtoupper($locale);
+                } else {
+                    $label = $this->translator()->translation($trans);
+                }
+            }
+
+            $isCurrent = ($locale === $currentLocale);
+            $languages[] = [
+                'ident'      => $locale,
+                'name'       => $label,
+            ];
+        }
+
+        return $languages;
+    }
+
+    /**
+     * @return string
+     */
+    public function availableLanguagesAsJson()
+    {
+        return json_encode($this->availableLanguages());
+    }
+
+    /**
+     * To be called with {@see uasort()}.
+     *
+     * @param  array $a Sortable action A.
+     * @param  array $b Sortable action B.
+     * @return integer
+     */
+    protected function sortLanguagesByPriority(array $a, array $b)
+    {
+        $a = isset($a['priority']) ? $a['priority'] : 0;
+        $b = isset($b['priority']) ? $b['priority'] : 0;
+
+        if ($a === $b) {
+            return 0;
+        }
+        return ($a < $b) ? (-1) : 1;
     }
 
     /**

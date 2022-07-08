1c7608f3993c831e68c1d524bf7400dc34b33035
Chauncey McAskill <chauncey@mcaskill.ca>
2022-07-08 15:04:36 -0400

    Refactor internals of attachment.js
    Repurposed `dirty` property to track any changes to the blocks:
    
    - When the sortable positions of blocks have changed.
    - When a block is added.
    
    Added `busy` property and methods to track when the widget is processing changes:
    
    - When blocks are being attached (joined) to a content model or parent block.
    - When a block is being dettached.
    
    Replaced "switch_language.charcoal" listener with listeners on new events for switching language:
    
    - "beforelanguageswitch" to check if widget is busy (and cancel the switch) and to check if widget is dirty (and save changes).
    - "languageswitch" (reload the widget).
    
    Split method `create_attachment()` into `create_quick_attachment()` and `create_dialog_attachment()` to decouple responsibilities.
    
    - `create_quick_attachment()` will return the `jqXHR` instance to allow implementors to continue thenable.
    - `create_dialog_attachment()` will return the `BootstrapDialog` instance to allow implementators to interact with it.
    
    Added:
    - Promise method `always()` to occurrences `$.post()` to delete the tracked XHR instance and mark widget as no longer busy. Previously, if the XHR request failed the widget would be in a blocked state.
    - Promise method `fail()` to occurrences `$.post()` to inform user of issue.
    - Method `change_locale()` to decouple responsibility.
    - Method `enqueue_is_busy_feedback()` to quickly push a warning that the widget is busy.
    - Method `validate()` to check if widget is busy, push warning that its busy, and cancel the saving process.
    - Method `perform_save()` to decouple responsibility.
    
    Changed:
    - Method `widget_options()` to preserve widget ID across reloads.

--- assets/dist/scripts/charcoal.admin.js
+++ assets/dist/scripts/charcoal.admin.js
@@ -3673,7 +3673,7 @@ Charcoal.Admin.Widget.prototype.confirm = function (dialog_opts, confirmed_callb
     return BootstrapDialog.confirm(opts);
 };
 
-/* globals commonL10n,attachmentWidgetL10n */
+/* globals attachmentWidgetL10n,commonL10n,formWidgetL10n,widgetL10n */
 
 /**
  * Keep track of XHR requests by group.
@@ -3694,6 +3694,9 @@ Charcoal.Admin.Widget_Attachment = function (opts) {
 
     Charcoal.Admin.Widget.call(this, opts);
 
+    this.busy  = false;
+    this.dirty = false;
+
     this.glyphs = {
         embed:      'glyphicon-blackboard',
         video:      'glyphicon-film',
@@ -3707,15 +3710,21 @@ Charcoal.Admin.Widget_Attachment = function (opts) {
     };
 
     var that = this;
-    $(document).on('switch_language.charcoal', function () {
-        var opts = that.opts();
-        // Set widget lang to current Charcoal Admin Lang
-        opts.widget_options.lang = Charcoal.Admin.lang();
-        that.set_opts(opts);
-        that.reload();
-    });
 
-    this.dirty = false;
+    $(document)
+        .on('beforelanguageswitch' + Charcoal.Admin.Widget_Form.EVENT_NAMESPACE, function (event) {
+            if (that.is_busy()) {
+                event.preventDefault();
+                that.enqueue_is_busy_feedback().dispatch();
+                return;
+            }
+
+            that.perform_save();
+        })
+        .on('languageswitch' + Charcoal.Admin.Widget_Form.EVENT_NAMESPACE, function () {
+            that.change_locale().try_reload();
+        });
+
     return this;
 };
 
@@ -3741,6 +3750,8 @@ Charcoal.Admin.Widget_Attachment.prototype.init = function () {
             $container.sortable('refreshPositions');
         });
 
+        var that = this;
+
         $container.sortable({
             handle:      '[draggable="true"]',
             placeholder: 'card c-attachments_row -placeholder',
@@ -3751,6 +3762,9 @@ Charcoal.Admin.Widget_Attachment.prototype.init = function () {
                 if (!$collapsible.hasClass('collapsed')) {
                     ui.item.children('.collapse').collapse('hide');
                 }
+            },
+            update:      function () {
+                that.set_dirty_state(true);
             }
         }).disableSelection();
     }
@@ -3760,21 +3774,58 @@ Charcoal.Admin.Widget_Attachment.prototype.init = function () {
 };
 
 /**
- * Check if the widget has something a dirty state that needs to be saved.
- * @return Boolean     Widget dirty of not.
+ * Checks whether the widget is busy.
+ *
+ * Checks if {@see Widget.is_reloading()} is TRUE or {@see this.busy} is TRUE.
+ *
+ * @return {boolean} TRUE if the widget is busy, otherwise FALSE.
+ */
+Charcoal.Admin.Widget_Attachment.prototype.is_busy = function () {
+    return (this.busy || this.is_reloading());
+};
+
+/**
+ * Sets whether the widget is busy.
+ *
+ * @param  {boolean} state
+ * @return {this}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.set_busy_state = function (state) {
+    this.busy = state;
+    return this;
+};
+
+/**
+ * Checks whether the widget has changes that should be saved.
+ *
+ * @return {boolean} TRUE if the widget is dirty, otherwise FALSE.
  */
 Charcoal.Admin.Widget_Attachment.prototype.is_dirty = function () {
     return this.dirty;
 };
 
 /**
- * Set the widget to dirty or not to prevent unnecessary save
- * action.
- * @param Boolean bool Self explanatory.
- * @return Add_Attachment_Widget Chainable.
+ * Sets whether the widget has changes that should be saved.
+ *
+ * @param  {boolean} state
+ * @return {this}
  */
-Charcoal.Admin.Widget_Attachment.prototype.set_dirty_state = function (bool) {
-    this.dirty = bool;
+Charcoal.Admin.Widget_Attachment.prototype.set_dirty_state = function (state) {
+    this.dirty = state;
+    return this;
+};
+
+/**
+ * Set widget lang to the given language or Charcoal's current language.
+ *
+ * @param  {string} [lang]
+ * @return {this}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.change_locale = function (lang) {
+    var opts = this.opts();
+    opts.widget_options.lang = (lang || Charcoal.Admin.lang());
+    this.set_opts(opts);
+
     return this;
 };
 
@@ -3950,7 +4001,32 @@ Charcoal.Admin.Widget_Attachment.prototype.select_attachment = function (elem) {
     }
 };
 
+/**
+ * @param  {string}        type
+ * @param  {number|string} [id]
+ * @param  {object}        [parent]
+ * @param  {object}        [customOpts]
+ * @param  {function}      [callback]
+ * @return {jqXHR|BootstrapDialog}
+ */
 Charcoal.Admin.Widget_Attachment.prototype.create_attachment = function (type, id, parent, customOpts, callback) {
+    // Skip quick form
+    if (customOpts && customOpts.skipForm) {
+        return this.create_quick_attachment(type, id, parent, customOpts, callback);
+    }
+
+    return this.create_dialog_attachment(type, id, parent, customOpts, callback);
+};
+
+/**
+ * @param  {string}        type
+ * @param  {number|string} [id]
+ * @param  {object}        [parent]
+ * @param  {object}        [customOpts]
+ * @param  {function}      [callback]
+ * @return {jqXHR}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.create_quick_attachment = function (type, id, parent, customOpts, callback) {
     // Id = EDIT mod.
     if (!id) {
         id = 0;
@@ -3960,42 +4036,83 @@ Charcoal.Admin.Widget_Attachment.prototype.create_attachment = function (type, i
         customOpts = {};
     }
 
-    // Scope
-    var that = this;
-
     if (!parent) {
-        var opts = that.opts();
-        parent   = {
-            obj_type: opts.data.obj_type,
-            obj_id:   opts.data.obj_id,
-            group:    opts.data.group
-        };
+        parent = this.get_parent_container();
     }
 
-    // Skip quick form
-    if (customOpts.skipForm) {
-        this.xhr = $.ajax({
-            type: 'POST',
-            url: 'object/save',
-            data: {
-                obj_type:  type,
-                obj_id:    id,
-                pivot:     parent
-            }
-        });
+    this.xhr = $.ajax({
+        type: 'POST',
+        url: 'object/save',
+        data: {
+            obj_type:  type,
+            obj_id:    id,
+            pivot:     parent
+        }
+    });
 
-        this.xhr.done(function (response) {
-            if (response.feedbacks) {
-                Charcoal.Admin.feedback(response.feedbacks).dispatch();
-            }
+    var success, failure, complete;
 
-            if (typeof callback === 'function') {
-                callback(response);
-            }
-        });
+    success = function (response) {
+        if (response.feedbacks.length) {
+            Charcoal.Admin.feedback(response.feedbacks);
+        }
+
+        if (typeof callback === 'function') {
+            callback(response);
+        }
+    };
+
+    failure = function (response) {
+        if (response.feedbacks.length) {
+            Charcoal.Admin.feedback(response.feedbacks);
+        } else {
+            var message = (id ? formWidgetL10n.updateFailed : formWidgetL10n.createFailed);
+            var error   = commonL10n.errorOccurred;
+
+            Charcoal.Admin.feedback([ {
+                level:   'error',
+                message: commonL10n.errorTemplate.replaceMap({
+                    '[[ errorMessage ]]': message,
+                    '[[ errorThrown ]]':  error
+                })
+            } ]);
+        }
+    };
+
+    complete = function () {
+        Charcoal.Admin.feedback().dispatch();
 
         Charcoal.Admin.manager().render();
-        return;
+    };
+
+    return Charcoal.Admin.resolveSimpleJsonXhr(
+        this.xhr,
+        success,
+        failure,
+        complete
+    );
+};
+
+/**
+ * @param  {string}        type
+ * @param  {number|string} [id]
+ * @param  {object}        [parent]
+ * @param  {object}        [customOpts]
+ * @param  {function}      [callback]
+ * @return {BootstrapDialog}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.create_dialog_attachment = function (type, id, parent, customOpts, callback) {
+    // Id = EDIT mod.
+    if (!id) {
+        id = 0;
+    }
+
+    if (!customOpts) {
+        customOpts = {};
+    }
+
+    if (!parent) {
+        parent = this.get_parent_container();
     }
 
     var defaultOpts = {
@@ -4016,46 +4133,68 @@ Charcoal.Admin.Widget_Attachment.prototype.create_attachment = function (type, i
     var immutableOpts = {};
     var dialogOpts = $.extend({}, defaultOpts, customOpts, immutableOpts);
 
-    var dialog = this.dialog(dialogOpts, function (response) {
-        if (response.success) {
-            // Call the quickForm widget js.
-            // Really not a good place to do that.
-            if (!response.widget_id) {
-                return false;
-            }
-
-            Charcoal.Admin.manager().add_widget({
-                id:   response.widget_id,
-                type: 'charcoal/admin/widget/quick-form',
-                data: response.widget_data,
-                obj_id: id,
-                save_callback: function (response) {
-                    if (typeof callback === 'function') {
-                        callback(response);
-                    }
-
-                    if ((this instanceof Charcoal.Admin.Component) && this.id()) {
-                        Charcoal.Admin.manager().destroy_component('widgets', this.id());
-                    }
-
-                    dialog.close();
-                }
-            });
-
-            // Re render.
-            // This is not good.
-            Charcoal.Admin.manager().render();
+    return this.dialog(dialogOpts, function (response, dialog) {
+        if (!response.success) {
+            return false;
         }
+
+        // Call the quickForm widget js.
+        // Really not a good place to do that.
+        if (!response.widget_id) {
+            return false;
+        }
+
+        Charcoal.Admin.manager().add_widget({
+            id:   response.widget_id,
+            type: 'charcoal/admin/widget/quick-form',
+            data: response.widget_data,
+            obj_id: id,
+            save_callback: function (response) {
+                if (typeof callback === 'function') {
+                    callback(response);
+                }
+
+                if ((this instanceof Charcoal.Admin.Component) && this.id()) {
+                    Charcoal.Admin.manager().destroy_component('widgets', this.id());
+                }
+
+                dialog.close();
+            }
+        });
+
+        // Re render.
+        // This is not good.
+        Charcoal.Admin.manager().render();
     });
+};
+
+/**
+ * @return {object}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.get_parent_container = function () {
+    var opts = this.opts();
+
+    return {
+        obj_type: opts.data.obj_type,
+        obj_id:   opts.data.obj_id,
+        group:    opts.data.group
+    };
 };
 
 /**
  * Add an attachment to an existing container.
  *
- * @param {object} attachment - The attachment to add to the container.
- * @param {object} container  - The container attachment.
+ * @param  {object} attachment - The attachment to add to the container.
+ * @param  {object} container  - The container attachment.
+ * @return {?jqXHR}
  */
 Charcoal.Admin.Widget_Attachment.prototype.add_object_to_container = function (attachment, container, grouping) {
+    if (this.is_busy()) {
+        return null;
+    }
+
+    this.set_busy_state(true);
+
     var that = this,
         data = {
             obj_type:    container.type,
@@ -4067,12 +4206,49 @@ Charcoal.Admin.Widget_Attachment.prototype.add_object_to_container = function (a
                     position: 0
                 }
             ],
-            group: grouping || container.group || ''
+            group: (grouping || container.group || '')
         };
 
-    $.post('add-join', data, function () {
+    if (globalXHR[data.group] != null && globalXHR[data.group].abort) {
+        globalXHR[data.group].abort();
+    }
+
+    var xhr = $.post('add-join', data);
+
+    var success, failure, complete;
+
+    success = function () {
         that.reload();
-    }, 'json');
+    };
+
+    failure = function (response) {
+        if (response.feedbacks.length) {
+            Charcoal.Admin.feedback(response.feedbacks);
+        } else {
+            Charcoal.Admin.feedback([ {
+                level:   'error',
+                message: commonL10n.errorTemplate.replaceMap({
+                    '[[ errorMessage ]]': formWidgetL10n.saveFailed,
+                    '[[ errorThrown ]]':  commonL10n.errorOccurred
+                })
+            } ]);
+        }
+    };
+
+    complete = function () {
+        delete globalXHR[data.group];
+
+        that.set_busy_state(false);
+
+        Charcoal.Admin.feedback().dispatch();
+    };
+
+    return globalXHR[data.group] = Charcoal.Admin.resolveSimpleJsonXhr(
+        xhr,
+        success,
+        failure,
+        complete
+    );
 };
 
 /**
@@ -4084,7 +4260,6 @@ Charcoal.Admin.Widget_Attachment.prototype.add = function (obj) {
         return false;
     }
 
-    // There is something to save.
     this.set_dirty_state(true);
 
     var template = this.element().find('.js-attachment-template').clone();
@@ -4092,6 +4267,39 @@ Charcoal.Admin.Widget_Attachment.prototype.add = function (obj) {
     this.element().find('.c-attachments_container > .js-grid-container').append(template);
 
     return this;
+};
+
+/**
+ * @return {Feedback}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.enqueue_is_busy_feedback = function () {
+    var opts = this.widget_options();
+    var widgetName = (opts && opts.title || attachmentWidgetL10n.widgetName);
+
+    return Charcoal.Admin.feedback([ {
+        level:   'warning',
+        display: 'toast',
+        message: commonL10n.errorTemplate.replaceMap({
+            '[[ errorMessage ]]': widgetName,
+            '[[ errorThrown ]]':  widgetL10n.isBusy
+        })
+    } ]);
+};
+
+/**
+ * Validates the component.
+ *
+ * @return {boolean}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.validate = function (scope) {
+    if (this.is_busy()) {
+        if (scope.attempts && scope.max_attempts && scope.attempts < scope.max_attempts) {
+            this.enqueue_is_busy_feedback();
+        }
+        return false;
+    }
+
+    return true;
 };
 
 /**
@@ -4117,19 +4325,41 @@ Charcoal.Admin.Widget_Attachment.prototype.add = function (obj) {
  * @return {boolean}
  */
 Charcoal.Admin.Widget_Attachment.prototype.save = function () {
-    if (this.is_dirty()) {
+    if (this.is_busy()) {
         return false;
     }
 
-    this.join();
+    return this.perform_save();
+};
+
+/**
+ * Performs the save operation.
+ *
+ * @return {boolean}
+ */
+Charcoal.Admin.Widget_Attachment.prototype.perform_save = function () {
+    if (this.is_dirty()) {
+        this.join();
+    }
 
     return true;
 };
 
+/**
+ * @param  {function}  [callback]
+ * @return {?jqXHR}
+ */
 Charcoal.Admin.Widget_Attachment.prototype.join = function (callback) {
-    if (!$('#' + this.element().attr('id')).length) {
-        return;
+    if (this.is_busy()) {
+        return null;
     }
+
+    if (!$('#' + this.element().attr('id')).length) {
+        return null;
+    }
+
+    this.set_busy_state(true);
+
     // Scope
     var that = this;
 
@@ -4154,31 +4384,67 @@ Charcoal.Admin.Widget_Attachment.prototype.join = function (callback) {
         });
     });
 
-    if (typeof globalXHR[opts.data.group] !== 'undefined') {
-        globalXHR[opts.data.group].abort();
+    if (globalXHR[data.group] != null && globalXHR[data.group].abort) {
+        globalXHR[data.group].abort();
     }
 
-    globalXHR[opts.data.group] = $.post('join', data, function () {
-        delete globalXHR[opts.data.group];
+    var xhr = $.post('join', data);
 
-        that.set_dirty_state(false);
+    var success, failure, complete;
 
+    success = function () {
         if (typeof callback === 'function') {
             callback();
         }
-    }, 'json');
+    };
+
+    failure = function (response) {
+        if (response.feedbacks.length) {
+            Charcoal.Admin.feedback(response.feedbacks);
+        } else {
+            Charcoal.Admin.feedback([ {
+                level:   'error',
+                message: commonL10n.errorTemplate.replaceMap({
+                    '[[ errorMessage ]]': formWidgetL10n.saveFailed,
+                    '[[ errorThrown ]]':  commonL10n.errorOccurred
+                })
+            } ]);
+        }
+    };
+
+    complete = function () {
+        delete globalXHR[data.group];
+
+        that.set_busy_state(false);
+        that.set_dirty_state(false);
+
+        Charcoal.Admin.feedback().dispatch();
+    };
+
+    return globalXHR[data.group] = Charcoal.Admin.resolveSimpleJsonXhr(
+        xhr,
+        success,
+        failure,
+        complete
+    );
 };
 
 /**
  * @param  {string|number} id
- * @param  {function}      callback
- * @return {boolean}
+ * @param  {function}      [callback]
+ * @return {?jqXHR}
  */
 Charcoal.Admin.Widget_Attachment.prototype.remove_join = function (id, callback) {
+    if (this.is_busy()) {
+        return null;
+    }
+
     if (!id) {
         // How could this possibly be!
-        return false;
+        return null;
     }
+
+    this.set_busy_state(true);
 
     // Scope
     var that = this;
@@ -4191,13 +4457,49 @@ Charcoal.Admin.Widget_Attachment.prototype.remove_join = function (id, callback)
         group:         opts.data.group
     };
 
-    $.post('remove-join', data, function () {
-        that.set_dirty_state(false);
+    if (globalXHR[data.group] != null && globalXHR[data.group].abort) {
+        globalXHR[data.group].abort();
+    }
 
+    var xhr = $.post('remove-join', data);
+
+    var success, failure, complete;
+
+    success = function () {
         if (typeof callback === 'function') {
             callback();
         }
-    }, 'json');
+    };
+
+    failure = function (response) {
+        if (response.feedbacks.length) {
+            Charcoal.Admin.feedback(response.feedbacks);
+        } else {
+            Charcoal.Admin.feedback([ {
+                level:   'error',
+                message: commonL10n.errorTemplate.replaceMap({
+                    '[[ errorMessage ]]': attachmentWidgetL10n.removeFailed,
+                    '[[ errorThrown ]]':  commonL10n.errorOccurred
+                })
+            } ]);
+        }
+    };
+
+    complete = function () {
+        delete globalXHR[data.group];
+
+        that.set_busy_state(false);
+        that.set_dirty_state(false);
+
+        Charcoal.Admin.feedback().dispatch();
+    };
+
+    return globalXHR[data.group] = Charcoal.Admin.resolveSimpleJsonXhr(
+        xhr,
+        success,
+        failure,
+        complete
+    );
 };
 
 /**
@@ -4205,7 +4507,14 @@ Charcoal.Admin.Widget_Attachment.prototype.remove_join = function (id, callback)
  * @return {[type]} [description]
  */
 Charcoal.Admin.Widget_Attachment.prototype.widget_options = function () {
-    return this.opts('widget_options');
+    var options = this.opts('widget_options');
+
+    // Hack to persist widget ID across reloads
+    if (!options.widget_id && this.widget_id()) {
+        options.widget_id = this.widget_id();
+    }
+
+    return options;
 };
 
 /* globals moment */

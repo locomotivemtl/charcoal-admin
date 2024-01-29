c69b6222db27fa7da6fb9e46caeb90128be0f564
Chauncey McAskill <chauncey@mcaskill.ca>
2024-01-29 18:43:39 -0500

    Remove default constructor descriptions
    They described what a constructor is in general and not what the classes' constructors do.

--- src/Charcoal/Admin/Service/AssetsBuilder.php
+++ src/Charcoal/Admin/Service/AssetsBuilder.php
@@ -24,17 +24,7 @@ final class AssetsBuilder
     private $am;
 
     /**
-     * PHP 5 allows developers to declare constructor methods for classes.
-     * Classes which have a constructor method call this method on each newly-created object,
-     * so it is suitable for any initialization that the object may need before it is used.
-     *
-     * Note: Parent constructors are not called implicitly if the child class defines a constructor.
-     * In order to run a parent constructor, a call to parent::__construct() within the child constructor is required.
-     *
-     * param [ mixed $args [, $... ]]
-     * @param array $data The init options.
      * @return void
-     * @link   http://php.net/manual/en/language.oop5.decon.php
      */
     public function __construct(array $data = [])
     {
--- src/Charcoal/Admin/Service/SelectizeRenderer.php
+++ src/Charcoal/Admin/Service/SelectizeRenderer.php
@@ -46,17 +46,8 @@ class SelectizeRenderer
     private $view;
 
     /**
-     * PHP 5 allows developers to declare constructor methods for classes.
-     * Classes which have a constructor method call this method on each newly-created object,
-     * so it is suitable for any initialization that the object may need before it is used.
-     *
-     * Note: Parent constructors are not called implicitly if the child class defines a constructor.
-     * In order to run a parent constructor, a call to parent::__construct() within the child constructor is required.
-     *
      * @param array $data Dependencies.
      * @throws Exception If missing dependencies.
-     * @return self
-     * @link http://php.net/manual/en/language.oop5.decon.php
      */
     public function __construct(array $data)
     {
